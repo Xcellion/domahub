d67b994be4b11b350345f91fa3a834ea6e1a826f
Wonmin Lee <won2blee@gmail.com>
2016-06-23 23:18:29 -0400

    trying 301

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -62,7 +62,6 @@ function sendRentalInfo(req, res, result){
 		var origin = req.headers.origin;
 		if (allowedOrigins.indexOf(origin) > -1){
 			res.setHeader('Access-Control-Allow-Origin', origin);
-			console.log(origin);
 		}
 		
 		switch (result.rental_info.type){
