b4fd112b7edfc236799036519bfd03acdf3f069c
Wonmin Lee <won2blee@gmail.com>
2016-07-17 16:12:15 -0400

    added ability to create new listings, fixed localhost/url issues

--- server/public/js/routes/client_profile.js
+++ server/public/js/routes/client_profile.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+
 	for (var x = 0; x < rentals.length; x++){
 		var tr = $("<tr></tr");
 		tr.append("<td>"+rentals[x].domain_name+"</td>");
@@ -8,4 +8,52 @@ $(document).ready(function() {
 		tr.append("<td><a href='/listing/" + rentals[x].domain_name + "/" + rentals[x].rental_id + "'>Rental details</a></td>");
 		$("#rental_table").append(tr);
 	}
+
+	$("#listing_form").submit(function(e){
+		e.preventDefault();
+		submitListings();
+	});
 });
+
+//function to sumibt listings
+function submitListings(){
+	$.ajax({
+		type: "POST",
+		url: "/profile",
+		data: {
+			domain_name: $("#listing_form_domain_name").val(),
+			description: $("#listing_form_description").val()
+		}
+	}).done(function(data){
+		if (data.state == "success"){
+			$("#message").html(data.message);
+		}
+		else if (data.state == "error"){
+			$("#message").html(data.message);
+		}
+		else {
+			console.log(data);
+		}
+	});
+}
+
+//function to set a domain as active
+function changeActive(domain_name){
+	$.ajax({
+		type: "POST",
+		url: "/profile/changeActive",
+		data: {
+			domain_name: domain_name,
+		}
+	}).done(function(data){
+		if (data.state == "success"){
+			$("#message").html(data.message);
+		}
+		else if (data.state == "error"){
+			$("#message").html(data.message);
+		}
+		else {
+			console.log(data);
+		}
+	});
+}
--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -4,20 +4,20 @@ var	account_model = require('../models/account_model.js'),
 module.exports = function(app, db, auth, e){
 	Auth = auth;
 	error = e;
-	
+
 	Account = new account_model(db);
 	Listing = new listing_model(db);
-	
+
 	//function to check if logged in
 	isLoggedIn = Auth.isLoggedIn;
 
 	//default page
 	app.get("/", mainPage);
-	app.get('/profile', isLoggedIn, profile);
 	app.get('/login', isLoggedIn);
 	app.get('/logout', Auth.logout);
 	app.get('/signup', Auth.signup);
-	
+
+	//posts for account
 	app.post('/signup', Auth.signupPost);
 	app.post('/login', Auth.loginPost);
 }
@@ -33,33 +33,4 @@ function mainPage(req, res, next){
 			});
 		}
 	});
-}
-
-//goes to profile
-function profile(req, res){
-	account_id = req.user.id;
-	
-	//check if user id is legit
-	if (parseFloat(account_id) == account_id >>> 0){
-		//get all available listings
-		Account.getListingsAccount(account_id, function(result){
-			listings = result.listings;
-			
-			//get all rentals for that user
-			Account.getRentalsAccount(account_id, function(result){
-				rentals = result.rentals;
-				
-				res.render("profile.ejs", {
-					message: Auth.messageReset(req),
-					user: req.user,
-					listings: listings,
-					rentals: rentals
-				});
-			});
-			
-		});
-	}
-	else {
-		error.handler(req, res, "Invalid user!");
-	}
 }
