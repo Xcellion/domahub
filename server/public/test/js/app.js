d82b5ffc18776cd6bc451c5dea9e7b40699b3c42
wonkyu <wonq33@gmail.com>
2016-07-20 17:26:12 -0400

    added log-in popup and log-in.html

--- server/public/test/js/app.js
+++ server/public/test/js/app.js
@@ -1,16 +1,23 @@
 var main = function() {
-  $('#login').click(function () {
-      $('#login_dropdown').toggle();
-      $('#session').toggleClass('active');
-      return false;
+
+  $('#login').leanModal({
+    top: 100,
+    overlay: 0.7,
+    closeButton: ".modal_close"
   });
-  $('#login_dropdown').click(function(e) {
-      e.stopPropagation();
-  });
-  $(document).click(function() {
-      $('#login_dropdown').hide();
-      $('#session').removeClass('active');
-  });
+
+  // $('#login').click(function () {
+  //     $('#login_dropdown').toggle();
+  //     $('#session').toggleClass('active');
+  //     return false;
+  // });
+  // $('#login_dropdown').click(function(e) {
+  //     e.stopPropagation();
+  // });
+  // $(document).click(function() {
+  //     $('#login_dropdown').hide();
+  //     $('#session').removeClass('active');
+  // });
 };
 
 $(document).ready(main);
