8b03fd9a12bf3303b34e9ec5be915230f2a3c8b5
Wonmin Lee <won2blee@gmail.com>
2016-07-21 19:01:56 -0400

    changed port to heroku standard

--- server/server.js
+++ server/server.js
@@ -85,8 +85,8 @@ server.listen(port, function(){
 
 var dnsd = require("dnsd");
 var server = dnsd.createServer(handler)
-server.zone('example.com', 'ns1.example.com', 'us@example.com', 'now', '2h', '30m', '2w', '10m').listen(5353)
-console.log('DNS server listening on 5353')
+server.zone('example.com', 'ns1.example.com', 'us@example.com', 'now', '2h', '30m', '2w', '10m').listen(port)
+console.log('DNS server listening on ' + port)
 
 function handler(req, res) {
 
