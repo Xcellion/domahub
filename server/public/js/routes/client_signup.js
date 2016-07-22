177a336182e11370550b46c3fcc4d5311bdb8d08
wonkyu <wonq33@gmail.com>
2016-07-21 22:30:52 -0400

    integrated login and register views

--- server/public/js/routes/client_signup.js
+++ server/public/js/routes/client_signup.js
@@ -1,41 +1,48 @@
 $(document).ready(function() {
+
+	// Calling Login Form
+	$("#login_link").click(function() {
+			$("#register_form").hide();
+			$("#login_form").show();
+			return false;
+	});
+
+	// Calling Register Form
+	$("#register_link").click(function() {
+			$("#login_form").hide();
+			$("#register_form").show();
+			return false;
+	});
+
 	$('#target').submit(function(event){
 		//if no email is entered
 		if (!$("#email_input").val()) {
-			$("#sub_message").fadeOut(100, function(){
-				$(this).css("color", "#ed1c24").html("Please enter your email address!").fadeIn(100);
-				$("#email_input").focus();
-			});
+			$("#message").html("Please enter your email address!");
+			$("#email_input").focus();
 			//to prevent submission of the form
 			return false;
 		}
-		
+
 		//if email is not valid
 		else if (!validateEmail($("#email_input").val())){
-			$("#sub_message").fadeOut(100, function(){
-				$(this).css("color", "#ed1c24").html("Please enter a valid email address!").fadeIn(100);
-				$("#email_input").focus();
-			});
+			$("#message").html("Please enter a valid email address!").fadeIn(100);
+			$("#email_input").focus();
 			//to prevent submission of the form
 			return false;
 		}
-		
+
 		//if no password is entered
 		else if (!$("#pw_input").val()) {
-			$("#sub_message").fadeOut(100, function(){
-				$(this).css("color", "#ed1c24").html("Please enter your password!").fadeIn(100);
-				$("#pw_input").focus();
-			});
+			$("message").html("Please enter your password!").fadeIn(100);
+			$("#pw_input").focus();
 			//to prevent submission of the form
 			return false;
 		}
-		
+
 		//if passwords do not match
 		else if ($("#pw_input").val() != $("#verify_input").val()){
-			$("#sub_message").fadeOut(100, function(){
-				$(this).css("color", "#ed1c24").html("Passwords do not match!").fadeIn(100);
-				$("#pw_input").focus();
-			});
+			$("#message").html("Passwords do not match!").fadeIn(100);
+			$("#pw_input").focus();
 			//to prevent submission of the form
 			return false;
 		}
