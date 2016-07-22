aa6cb3cf7a751aefa88c9dc0ccc07f6068d30f13
Wonmin Lee <won2blee@gmail.com>
2016-07-21 21:05:18 -0400

    removed heroku ports

--- server/server.js
+++ server/server.js
@@ -81,14 +81,14 @@ app.get('*', function(req, res){
 	res.redirect('/');
 });
 
-var port = Number(process.env.PORT || 80);
+var port = Number(80);
 server(app).listen(port, function(){
 	console.log("Listening on port " + port);
 });
 
 var dnsd = require("dnsd");
 var server = dnsd.createServer(handler)
-server.zone('example.com', 'ns1.example.com', 'us@example.com', 'now', '2h', '30m', '2w', '10m').listen(process.env.PORT || 53)
+server.zone('example.com', 'ns1.example.com', 'us@example.com', 'now', '2h', '30m', '2w', '10m').listen(53)
 console.log('DNS server listening on 53')
 
 function handler(req, res) {
