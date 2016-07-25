53bda25d8f084145629912cf20a3b40b32e6f9d2
wonq33 <wonq33@gmail.com>
2016-07-25 16:16:39 -0400

    changes to sellers view

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,9 @@
 // set up paths
 module.exports = function(app, db, auth, error){
-	require('./companyinfo.js')(app, db, auth, error); // For all company related views located in footer
 	require('./accounts.js')(app, db, auth, error); // For authentication, account creation, log in, log out
-	require('./profiles.js')(app, db, auth, error); // For individual user profiles
+	require('./companyinfo.js')(app, db, auth, error); // For all company related views located in footer
 	require('./listings.js')(app, db, auth, error); // For website listings
+	require('./profiles.js')(app, db, auth, error); // For individual user profiles
 	require('./rentals.js')(app, db, auth, error); //
+	require('./sellers.js')(app, db, auth, error); // Sellers information page
 }
