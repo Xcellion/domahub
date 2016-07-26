7621634b8396ee5798bc0f6b94c0989eefe5fbf8
Wonmin Lee <won2blee@gmail.com>
2016-07-26 15:21:18 -0400

    testing port 80 listen

--- server/server.js
+++ server/server.js
@@ -85,8 +85,8 @@ app.get('*', function(req, res){
 });
 
 //main website on port 8080
-server(app).listen(8080, function(){
-	console.log("Main website listening on port 8080");
+server(app).listen(80, function(){
+	console.log("Main website listening on port 80");
 });
 
 // //sub-domain router reverse proxy
