3ae4e0a04a6fedee8aaa160cfe15ab644ff645fc
wonkyu <wonq33@gmail.com>
2016-08-01 19:05:43 -0400

    added routes for the footer links, added basic view pages

---
+++ server/routes/careers.js
@@ -0,0 +1,31 @@
+var	account_model = require('../models/account_model.js'),
+	listing_model = require('../models/listing_model.js');
+
+module.exports = function(app, db, auth, e){
+	Auth = auth;
+	error = e;
+
+	Account = new account_model(db);
+	Listing = new listing_model(db);
+
+	//function to check if logged in
+	isLoggedIn = Auth.isLoggedIn;
+
+	//default page
+	app.get("/careers", careersPage);
+	app.get('/login', isLoggedIn);
+	app.get('/logout', Auth.logout);
+	app.get('/signup', Auth.signup);
+
+	//posts for account
+	app.post('/signup', Auth.signupPost);
+	app.post('/login', Auth.loginPost);
+}
+
+//display about page
+function careersPage(req, res, next){
+	res.render("careers", {
+		message: Auth.messageReset(req),
+		user: req.user
+	});
+}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,13 @@ module.exports = function(app, db, auth, error, proxy){
 	require('./api/api.js')(app, db, error, proxy);			// API for all domains listed on w3bbi
 
 	require('./accounts.js')(app, db, auth, error); 		// For authentication, account creation, log in, log out
-	require('./companyinfo.js')(app, db, auth, error); 		// For all company related views located in footer
+	require('./careers.js')(app, db, auth, error); 			// Careers page
+	require('./companyinfo.js')(app, db, auth, error);  // For all company related views located in footer
+	require('./contact.js')(app, db, auth, error); 			// Contact Us page
 	require('./listings.js')(app, db, auth, error); 		// For website listings
 	require('./profiles.js')(app, db, auth, error); 		// For individual user profiles
 	require('./rentals.js')(app, db, auth, error); 			//
 	require('./sellers.js')(app, db, auth, error); 			// Sellers information page
-	require('./contact.js')(app, db, auth, error); 			// Contact Us page
+	require('./tos.js')(app, db, auth, error); 					// Terms of Service
+	require('./privacy.js')(app, db, auth, error); 			// Privacy Policy
 }
