f596ea24248a96376c917f5f72ab24caa34968fd
Wonmin Lee <won2blee@gmail.com>
2016-09-12 12:03:25 -0400

    get started logic

--- server/public/js/routes/getstarted_logic.js
+++ server/public/js/routes/getstarted_logic.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-
+    $(".selection").click(function(e){
+        $(this).parents(".section").addClass("is-hidden");
+        $(this).parents(".section").next(".section").removeClass("is-hidden");
+    })
 
 });
