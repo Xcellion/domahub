e182a02c6b5c92ae7fdd75880c78a17a6823254f
Wonmin Lee <won2blee@gmail.com>
2016-09-22 19:55:22 -0400

    inbox create new msg

--- server/public/js/routes/profile_inbox_logic.js
+++ server/public/js/routes/profile_inbox_logic.js
@@ -1,8 +1,35 @@
 var can_submit = true;
+var chat_panels = convo_list.slice();
 
 $(document).ready(function() {
 
-    createConvos();
+    createPanel(chat_panels);     //create the convo list panel on the left
+
+    //for searching for a specific user
+    $("#search-user").keyup(function(e){
+        var needle = $(this).val();
+
+        if (needle){
+            var temp_rows = [];
+            for (var x = 0; x < convo_list.length; x++){
+                temp_username = convo_list[x].username
+                if (temp_username.toLowerCase().includes(needle.toLowerCase())){
+                    temp_rows.push(convo_list[x]);
+                }
+            }
+            chat_panels = temp_rows;
+            createPanel(chat_panels);
+        }
+        else {
+            chat_panels = convo_list.slice();
+            createPanel(chat_panels);
+        }
+    });
+
+    //create a new message button
+    $("#new-message").click(function(e){
+        changeConvo();
+    })
 
     //enter to submit
     $("#msg_text_input").keypress(function(e){
@@ -12,15 +39,10 @@ $(document).ready(function() {
         }
     });
 
-    //new message button
-    $("#new-message").click(function(e){
-        console.log('s')
-    })
-
     //submission of a new msg
     $("#messenger_form").submit(function(e){
         e.preventDefault();
-        submit_data = checkMessage();
+        submit_data = checkSubmitFormat();
 
         if (can_submit && submit_data){
             can_submit = false;
@@ -30,12 +52,26 @@ $(document).ready(function() {
     			data: submit_data
     		}).done(function(data){
     			if (data.state == "success"){
+                    var existing = false;
+                    for (var x = 0; x < convo_list.length; x++){
+                        if (convo_list[x].username.toLowerCase() == submit_data.msg_receiver.toLowerCase()){
+                            existing = convo_list[x];
+                            break;
+                        }
+                    }
+
+                    //if its an existing convo, change to it
+                    if (existing){
+                        changeConvo(existing);
+                    }
+
+                    //append new message
                     $("#chat_wrapper").append(createConvoMsg({
-                        message: $("#msg_text_input").val()
+                        message: submit_data.msg_text
                     }, false));
-                    $('#chat_wrapper').scrollTop($('#chat_wrapper')[0].scrollHeight);
+                    can_submit = true;
                     $("#msg_text_input").val("");
-        			can_submit = true;
+                    $('#chat_wrapper').scrollTop($('#chat_wrapper')[0].scrollHeight);
     			}
     			else if (data.state == "error"){
                     console.log(data.message);
@@ -54,15 +90,17 @@ $(document).ready(function() {
 //--------------------------------------------------------------------------------SUBMIT
 
 //client side function to check message format
-function checkMessage(){
+function checkSubmitFormat(){
     msg_receiver = $("#msg_receiver_input").val();
     msg_text = $("#msg_text_input").val();
 
     if (!msg_receiver){
         console.log("invalid person");
+        //todo
     }
     else if (!msg_text){
         console.log("no message");
+        //todo
     }
     else {
         return {
@@ -77,38 +115,18 @@ function checkMessage(){
 //--------------------------------------------------------------------------------DISPLAY
 
 //function to create all chat convos on the left
-function createConvos(){
-    if (chat_history){
-        //side panel convo list
-        for (var x = 0; x < chat_history.length; x++){
-            $(".panel-list").append(createPanelConvo(chat_history[x]));
-        }
+function createPanel(chat_panels){
+    $(".panel-list").empty();
+    //side panel convo list
+    for (var x = 0; x < chat_panels.length; x++){
+        $(".panel-list").append(createPanelConvo(chat_panels[x]));
     }
 }
 
 //function to display a single chat convo on the left
 function createPanelConvo(convo_item){
     var latest_time = moment(new Date(convo_item.timestamp + "Z"));
-    var moment_now = moment(new Date());
-    var diff_to_now = moment_now.diff(latest_time);
-    var diff_to_lny = latest_time.diff(moment(new Date("01/01/" + (moment_now.year() - 1))));
-
-    //less than a day
-    if (diff_to_now < 86400000){
-        var disp_time = latest_time.format("h:mma");
-    }
-    //less than a week
-    else if (diff_to_now < 604800000){
-        var disp_time = latest_time.format("ddd");
-    }
-    //less than last year
-    else if (diff_to_now < diff_to_lny){
-        var disp_time = latest_time.format("MMM DD");
-    }
-    //greater than last year
-    else {
-        var disp_time = latest_time.format("YYYY/MM/DD");
-    }
+    disp_time = formatTimestamp(latest_time);
 
     var panel_block = $("<a class='panel-block'></a>");
         var panel_icon = $("<span class='panel-icon'></span>");
@@ -121,20 +139,30 @@ function createPanelConvo(convo_item){
         $.ajax({
             type: "POST",
             url: "/messages/" + convo_item.username
-        }).done(function(data){
-            changeConvo(data);
+        }).done(function(chat_item){
+            changeConvo(appendChatsToConvo(chat_item));
         });
     })
 
     return panel_block.append(panel_icon.append(panel_i), name, panel_p);
 }
 
+//function to append chat msgs to convo list
+function appendChatsToConvo(chat_item){
+    for (var x = 0; x < convo_list.length; x++){
+        if (convo_list[x].username == chat_item.username){
+            convo_list[x].chats = chat_item.chats;
+            return convo_list[x];
+        }
+    }
+}
+
 //function to change selected convo
 function changeConvo(convo_item){
-    console.log(convo_item)
     $("#chat_wrapper").empty();
+    $("#msg_text_input").val("");
 
-    //changing to a conversation
+    //changing to an existing conversation
     if (convo_item){
         for (var x = 0; x < convo_item.chats.length; x++){
             $("#chat_wrapper").append(createConvoMsg(convo_item.chats[x], true));
@@ -142,9 +170,9 @@ function changeConvo(convo_item){
         $("#msg_receiver_input").val(convo_item.username).addClass("is-disabled");
         $('#chat_wrapper').scrollTop($('#chat_wrapper')[0].scrollHeight);
     }
-    //changing to a totally new convo
+    //changing to a totally new convo (create new msg)
     else {
-
+        $("#msg_receiver_input").val("").removeClass("is-disabled");
     }
 }
 
@@ -155,37 +183,21 @@ function createConvoMsg(chat_item, bool){
         var timestamp = moment(new Date(chat_item.timestamp + "Z")).format("M/DD, H:mma");
         var send_or_not = (chat_item.sender_account_id == user.id) ? "sender_me" : "sender_them";
     }
+    //came from client
     else {
         var timestamp = moment(new Date()).format("M/DD, H:mma");
         var send_or_not = "sender_me";
     }
 
     var latest_time = (chat_item.timestamp) ? moment(new Date(chat_item.timestamp + "Z")) : moment(new Date());
-    var moment_now = moment(new Date());
-    var diff_to_now = moment_now.diff(latest_time);
-    var diff_to_lny = latest_time.diff(moment(new Date("01/01/" + (moment_now.year() - 1))));
-
-    //less than a day
-    if (diff_to_now < 86400000){
-        var disp_time = latest_time.format("h:mma");
-    }
-    //less than a week
-    else if (diff_to_now < 604800000){
-        var disp_time = latest_time.format("ddd");
-    }
-    //less than last year
-    else if (diff_to_now < diff_to_lny){
-        var disp_time = latest_time.format("MMM DD");
-    }
-    //greater than last year
-    else {
-        var disp_time = latest_time.format("YYYY/MM/DD");
-    }
+    disp_time = formatTimestamp(latest_time);
 
     var temp_div = $("<div class='message_wrapper " + send_or_not + "'></div>");
-        var temp_msg = $("<p class='chat_message'>" + chat_item.message + "</p>");
+        var temp_msg = $("<p class='chat_message'></p>");
+            temp_msg.text(chat_item.message);
         var temp_timestamp = $("<p class='chat_timestamp'>" + disp_time + "</p>");
 
+    //change append order depending on who sent the msg
     if (send_or_not == "sender_me"){
         temp_div.append(temp_timestamp, temp_msg);
     }
@@ -194,4 +206,28 @@ function createConvoMsg(chat_item, bool){
     }
 
     return temp_div;
+}
+
+//helper function to format timestamp
+function formatTimestamp(latest_time){
+    var moment_now = moment(new Date());
+    var diff_to_now = moment_now.diff(latest_time);
+    var diff_to_lny = latest_time.diff(moment(new Date("01/01/" + (moment_now.year() - 1))));
+
+    //less than a day
+    if (diff_to_now < 86400000){
+        return latest_time.format("h:mma");
+    }
+    //less than a week
+    else if (diff_to_now < 604800000){
+        return latest_time.format("ddd");
+    }
+    //less than last year
+    else if (diff_to_now < diff_to_lny){
+        return latest_time.format("MMM DD");
+    }
+    //greater than last year
+    else {
+        return latest_time.format("YYYY/MM/DD");
+    }
 }
