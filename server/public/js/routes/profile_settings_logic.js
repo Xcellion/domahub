f2705313d8932aab15a03b970f5f99635ba1d530
Wonmin Lee <won2blee@gmail.com>
2016-09-22 21:23:06 -0400

    setting scroll works

--- server/public/js/routes/profile_settings_logic.js
+++ server/public/js/routes/profile_settings_logic.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+
+    //scroll to specific part of page depending on which side link you pressed
+    $(".setting-link").click(function(e){
+        temp_id = $(this).attr("id");
+        console.log($("#" + temp_id.substr(0, temp_id.length - 5)));
+        $("html, body").stop().animate({
+            scrollTop: $("#" + temp_id.substr(0, temp_id.length - 5)).offset().top - 70
+        }, "fast");
+    })
 });
