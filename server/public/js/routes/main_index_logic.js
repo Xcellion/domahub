1c70c7eba59be689047d3d4b0baa319a3e2722eb
wonkyu <wonq33@gmail.com>
2016-09-30 19:45:55 -0400

    idk

--- server/public/js/routes/main_index_logic.js
+++ server/public/js/routes/main_index_logic.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $('.index-link').hover(function() {
-        $(this).next().find('.right-arrow').toggleClass('active');
-    });
+    // $('.index-link').hover(function() {
+    //     $(this).next().find('.right-arrow').toggleClass('active');
+    // });
 
     //right and left ticker arrows
-    $(".carousel-icon").click(function(e){
-        if ($(this).hasClass("left")){
-            $('.carousel-wrapper').slick('slickPrev');
-        }
-        else {
-            $('.carousel-wrapper').slick('slickNext');
-        }
-    });
+    // $(".carousel-icon").click(function(e){
+    //     if ($(this).hasClass("left")){
+    //         $('.carousel-wrapper').slick('slickPrev');
+    //     }
+    //     else {
+    //         $('.carousel-wrapper').slick('slickNext');
+    //     }
+    // });
 
     //slick carousel
     $('.carousel-wrapper').slick({
