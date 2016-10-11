f93fb9a5120d52f0bbc99be641cdd9aaabe9f49a
wonkyu <wonq33@gmail.com>
2016-10-11 17:35:47 -0400

    added faq view, still needs some content

--- server/public/js/routes/faq_logic.js
+++ server/public/js/routes/faq_logic.js
@@ -12,7 +12,22 @@ $(document).ready(function() {
       $(this).addClass("is-active");
       $(this).next().removeClass("is-hidden");
     }
-    
+
   });
 
 });
+
+// $(".faq-question").on(("click, keydown",function(e) {
+//
+//   if (e.keyCode == 13) {
+//     $(".faq-question").removeClass("is-active");
+//     $(".faq-question").next().addClass("is-hidden");
+//
+//     if ($(this).hasClass("is-active")) {
+//       $(this).removeClass("is-active");
+//       $(this).next().addClass("is-hidden");
+//     } else {
+//       $(this).addClass("is-active");
+//       $(this).next().removeClass("is-hidden");
+//     }
+//   }
