2275b2d3f70aba6846ce05d626e49b3e0c49af05
Wonmin Lee <won2blee@gmail.com>
2016-10-12 03:17:36 -0400

    premium and basic working

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,8 @@
-module.exports = function(app, db, auth, error){
-	require('./main_routes.js')(app, db, auth, error); 				// for all main page links
-	require('./auth_routes.js')(app, auth); 						// for authentication, account creation, log in, log out
-	require('./listing_routes.js')(app, db, auth, error); 			// for website listings
-	require('./profile_routes.js')(app, db, auth, error); 			// for individual user profiles
-	require('./chat_routes.js')(app, db, auth, error); 				// for messaging
-	require('./stripe_routes.js')(app); 							// for stripe web hooks
+module.exports = function(app, db, auth, error, stripe){
+	require('./main_routes.js')(app, db, auth, error); 						// for all main page links
+	require('./auth_routes.js')(app, auth); 								// for authentication, account creation, log in, log out
+	require('./listing_routes.js')(app, db, auth, error, stripe); 			// for website listings
+	require('./profile_routes.js')(app, db, auth, error, stripe); 			// for individual user profiles
+	require('./chat_routes.js')(app, db, auth, error); 						// for messaging
+	require('./stripe_routes.js')(app, db, stripe); 						// for stripe web hooks
 }
