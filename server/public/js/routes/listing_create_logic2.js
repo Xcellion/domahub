ec690f43e16fd9701e87de1db37f88d58d66f37d
wonkyu <wonq33@gmail.com>
2016-10-14 23:28:46 -0400

    fixed some things w the checkboxes

--- server/public/js/routes/listing_create_logic2.js
+++ server/public/js/routes/listing_create_logic2.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $("#next-button").click(function() {
+  $(".next-button").click(function() {
     $(this).parents(".section").addClass("is-hidden");
     $(this).parents(".section").next(".section").removeClass("is-hidden");
   });
