678b0754b7755734a3708e312d37c54b3352a7e6
wonkyu <wonq33@gmail.com>
2016-10-16 15:57:35 -0400

    added TOS, Privacy Policy, and Create listing preliminary views

--- server/public/js/routes/listing_create_logic2.js
+++ server/public/js/routes/listing_create_logic2.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+  $(".box").click(function() {
+    $(".box").removeClass("is-active");
+
+    if ($(this).hasClass("is-active")) {
+      $(this).removeClass("is-active");
+    } else {
+      $(this).addClass("is-active");
+    }
+  });
+
   $(".next-button").click(function() {
     $(this).parents(".section").addClass("is-hidden");
     $(this).parents(".section").next(".section").removeClass("is-hidden");
