43dd6f1064eea62d47f52f8fd0de3e55bd7e1151
Wonmin Lee <won2blee@gmail.com>
2016-10-18 15:18:10 -0400

    listing create logic

--- server/public/js/routes/listing_create_logic2.js
+++ server/public/js/routes/listing_create_logic2.js
@@ -10,13 +10,24 @@ $(document).ready(function() {
     }
   });
 
+  //next button for sections
   $(".next-button").click(function() {
-    $(this).parents(".section").addClass("is-hidden");
-    $(this).parents(".section").next(".section").removeClass("is-hidden");
+    var current_section = $(".current-section");
+    var next_section = $(".current-section").next(".section");
+    current_section.addClass("is-hidden").removeClass("current-section")
+    next_section.addClass("current-section").removeClass("is-hidden");
+  });
+
+  //next button for sections
+  $(".prev-button").click(function() {
+    var current_section = $(".current-section");
+    var prev_section = $(".current-section").prev(".section");
+    current_section.addClass("is-hidden").removeClass("current-section")
+    prev_section.addClass("current-section").removeClass("is-hidden");
   });
 
   $(".fa-question-circle-o").hover(function(e){
       console.log("more info pls");
-  })
+  });
 
 });
