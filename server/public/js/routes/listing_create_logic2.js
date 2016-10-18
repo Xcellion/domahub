ed386e66e2ed5845d26be9fbe1a2880c6d3e1ef1
Wonmin Lee <won2blee@gmail.com>
2016-10-18 17:57:01 -0400

    listing preview for listing create

--- server/public/js/routes/listing_create_logic2.js
+++ server/public/js/routes/listing_create_logic2.js
@@ -3,14 +3,45 @@ $(document).ready(function() {
     //section 1 - basic vs premium
     $(".box").click(function() {
         $(".box").removeClass("is-active");
+        $(this).addClass("is-active");
+        $("#next-button").removeClass("is-disabled");
 
-        //set the data of the section so we can go next
-        $(this).closest('.current-section').data("can-next", true);
+        //to skip the pricing section or not depending on the listing type
+        if ($(this).data("listing-type") == "basic"){
+            $("#pricing-section").data('pageskip', true);
+        }
+        else {
+            $("#pricing-section").data('pageskip', false);
+        }
+    });
 
-        if ($(this).hasClass("is-active")) {
-            $(this).removeClass("is-active");
-        } else {
-            $(this).addClass("is-active");
+    //section 2 - listing info
+    $(".required-input").on("keydown, keyup", function(e){
+        e.preventDefault();
+        if ($("#sing-domain").val() && $("#sing-description").val()){
+            $("#next-button").removeClass("is-disabled");
+
+            //update the listing preview
+            $("#preview_domain").text($("#sing-domain").val());
+            $("#preview_description").text($("#sing-description").val());
+        }
+        else {
+            $("#next-button").addClass("is-disabled");
+        }
+    });
+
+    //if theres an error in getting the image, remove the link
+    $("#preview_image").error(function() {
+        $(this).attr("src", "https://source.unsplash.com/category/people");
+    });
+
+    //section 3 - categories
+    $(".cat-checkbox-label, .cat-checkbox").on("click", function(e){
+        if ($('.cat-checkbox:checkbox:checked').length > 0){
+            $("#next-button").removeClass("is-disabled");
+        }
+        else {
+            $("#next-button").addClass("is-disabled");
         }
     });
 
@@ -18,7 +49,7 @@ $(document).ready(function() {
     $(".next-button").click(function() {
         var current_section = $(".current-section");
         var next_section = $(".current-section").next(".section");
-        changePage(current_section, next_section);
+        changePage(current_section, next_section, false);
     });
 
     //previous button for sections
@@ -28,66 +59,51 @@ $(document).ready(function() {
         changePage(current_section, prev_section, true);
     });
 
+    //submit button
+    $("#submit-button").click(function(e){
+
+    });
+
     //more info tooltips
-    $(".fa-question-circle-o").hover(function(e){
+    $(".fa-question-circle-o").click(function(e){
         console.log("more info pls");
     });
 
 });
 
 //function to change pages and add disabled to buttons
-function changePage(current_page, upcoming_page, bool){
-    if (canNext(current_page) == true || bool){
-        if (upcoming_page.data('num') == "1"){
-            $("#prev-button").addClass("is-disabled");
-        }
-        else if (upcoming_page.data('num') == "5"){
-            $("#next-button").addClass("is-disabled");
-        }
-        else {
-            $("#prev-button, #next-button").removeClass("is-disabled");
-        }
-
-        current_page.addClass("is-hidden").removeClass("current-section");
-        upcoming_page.addClass("current-section").removeClass("is-hidden");
+function changePage(current_page, upcoming_page, prev){
+    //skip the price page if its a basic listing
+    if (upcoming_page.data("pageskip") == true){
+        upcoming_page = (prev) ? upcoming_page.prev(".section") : upcoming_page.next(".section");
     }
-}
 
-//function to check if we can go next
-function canNext(current_page){
-    var bool = true;
-    switch (current_page.data('pagenum')){
-        case 1:
-            if (current_page.data("can-next") != true){
-                bool = false;
-                current_page.find(".error-heading").text("You must select a listing type!").addClass('is-danger');
-            }
-            else {
-                current_page.find(".error-heading").text("Please select one of the options below").removeClass('is-danger');
-            }
-            break;
-        case 2:
-            if ($("#sing-domain").val() == ""){
-                bool = false;
-                $("#domain-error-message").text("You must enter a domain name!").addClass("is-danger");
-                $("#description-error-message").text("Description").removeClass("is-danger");
-            }
-            else if ($("#sing-description").val() == ""){
-                bool = false;
-                $("#domain-error-message").text("Domain Name").removeClass("is-danger");
-                $("#description-error-message").text("You must enter a description!").addClass("is-danger");
-            }
-            else {
-                $("#domain-error-message").text("Domain Name").removeClass("is-danger");
-                $("#description-error-message").text("Description").removeClass("is-danger");
-            }
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
+    var nextpagenum = upcoming_page.data('pagenum');
+
+    //first page, disable previous
+    if (nextpagenum == 1){
+        $("#prev-button").addClass("is-disabled");
     }
-    return bool;
+    else if (nextpagenum != 4){
+        $("#next-button").addClass("is-disabled");
+        $("#prev-button").removeClass("is-disabled");
+    }
+
+    //remove next disable if we went back
+    if (prev){
+        $("#next-button").removeClass("is-disabled");
+    }
+
+    //last page, hide buttons
+    if (nextpagenum == 5){
+        $("#next-button, #prev-button").addClass('is-hidden');
+        $("#submit-button").removeClass('is-hidden');
+    }
+    else {
+        $("#next-button, #prev-button").removeClass('is-hidden');
+        $("#submit-button").addClass('is-hidden');
+    }
+
+    current_page.addClass("is-hidden").removeClass("current-section");
+    upcoming_page.addClass("current-section").removeClass("is-hidden");
 }
