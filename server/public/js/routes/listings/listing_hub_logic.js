7e5827afd0d293b87aebd4ba5e4f5ceae95747d6
wonkyu <wonq33@gmail.com>
2016-10-20 12:27:30 -0400

    created template for js scripts and subsequently fixed all ejs pages to have scripts at the end of the body

--- server/public/js/routes/listings/listing_hub_logic.js
+++ server/public/js/routes/listings/listing_hub_logic.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+  var slider = document.getElementById('slider');
+
+  noUiSlider.create(slider, {
+  	start: [0, 5000],
+  	connect: true,
+  	range: {
+  		'min': 0,
+  		'max': 5000
+  	}
+  });
+
 });
