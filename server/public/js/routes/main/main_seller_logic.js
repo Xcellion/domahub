672e6ed3afa2b2633989c3816edce8228336d535
Wonmin Lee <won2blee@gmail.com>
2016-10-20 01:07:29 -0400

    moved all client side JS

--- server/public/js/routes/main/main_seller_logic.js
+++ server/public/js/routes/main/main_seller_logic.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     $(".earnings-input").on('change keyup paste mousewheel',function(e){
         calculateEarnings();
-    })
+    });
 
 });
 
--- server/routes/listings/listings_stats_routes.js
+++ server/routes/listings/listings_stats_routes.js
@@ -1,4 +1,4 @@
-var	listing_model = require('../models/listing_model.js');
+var	listing_model = require('../../models/listing_model.js');
 
 module.exports = {
 
