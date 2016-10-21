5389c2502894bdd535a5ec544c4322e6de4e3c72
wonkyu <wonq33@gmail.com>
2016-10-21 14:08:41 -0400

    pushing listing create changes

--- server/public/js/routes/listings/listing_hub_logic.js
+++ server/public/js/routes/listings/listing_hub_logic.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   noUiSlider.create(slider, {
   	start: [0, 5000],
   	connect: true,
+    margin: 1,
+    behaviour: 'drag-snap',
   	range: {
   		'min': 0,
   		'max': 5000
