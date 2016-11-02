5fb02b5d890b19280fb0254d7d5fec55b66385d7
Wonmin Lee <won2blee@gmail.com>
2016-11-02 00:52:08 -0400

    changing navbar among other

--- server/public/js/routes/main/main_faq_logic.js
+++ server/public/js/routes/main/main_faq_logic.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     $(".menu-link").click(function(){
         $(".menu-link").removeClass("is-active");
         $(this).addClass("is-active");
+        var faq_id = $(this).attr("id").split("-")[0];
+        $("#" + faq_id).find(".faq-header").removeClass("is-active");
+        $("#" + faq_id).find(".qa-wrapper").removeClass("is-hidden");
     });
 
     //if you came direct to url with a hash
