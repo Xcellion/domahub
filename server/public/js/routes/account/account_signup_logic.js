1f5584288a1294ff97ff341f342fd272704ade6b
Wonmin Lee <won2blee@gmail.com>
2016-11-04 17:01:27 -0400

    contact form post

--- server/public/js/routes/account/account_signup_logic.js
+++ server/public/js/routes/account/account_signup_logic.js
@@ -68,61 +68,11 @@ $(document).ready(function() {
 		}
 	});
 
-	//to catch empty emails, name, or passwords
+	//to catch captcha empty
 	$('#target').submit(function(event){
 
-		//if no email is entered
-		if (!$("#email-input").val()) {
-			$("#message").fadeOut(100, function(){
-				$("#message").css("color", "#ed1c24").text("Please enter your email address!").fadeIn(100);
-				$("#email-input").focus();
-				showSuccessDanger($("#email-input"), false);
-			});
-			return false;
-		}
-
-		//if no name is entered
-		else if (!$("#username-input").val() || $("#username-input").val().length > 70 || $("#username-input").val().includes(" ")) {
-			$("#message").fadeOut(100, function(){
-				$("#message").css("color", "#ed1c24").text("Please enter a valid username!").fadeIn(100);
-				$("#username-input").focus();
-				showSuccessDanger($("#username-input"), false);
-			});
-			return false;
-		}
-
-		//if no password is entered
-		else if (!$("#pw-input").val()) {
-			$("#message").fadeOut(100, function(){
-				$("#message").css("color", "#ed1c24").text("Please enter a password!").fadeIn(100);
-				$("#pw-input").focus();
-				showSuccessDanger($("#pw-input"), false);
-			});
-			return false;
-		}
-
-		//if password is too short or long
-		else if ($("#pw-input").val().length > 70 || $("#pw-input").val().length < 6) {
-			$("#message").fadeOut(100, function(){
-				$("#message").css("color", "#ed1c24").text("Please enter a valid password!").fadeIn(100);
-				$("#pw-input").focus();
-				showSuccessDanger($("#pw-input"), false);
-			});
-			return false;
-		}
-
-		//if passwords do not match
-		else if ($("#pw-input").val() != $("#verify-pw").val()){
-			$("#message").fadeOut(100, function(){
-				$("#message").css("color", "#ed1c24").html("Passwords do not match!").fadeIn(100);
-				$("#pw-input").focus();
-				showSuccessDanger($("#verify-pw"), false);
-			});
-			return false;
-		}
-
 		//if recaptcha is not done
-		else if (validateform()){
+		if (!validateCaptcha()){
 			$("#message").fadeOut(100, function(){
 				$("#message").css("color", "#ed1c24").html("Please prove you're not a robot!").fadeIn(100);
 			});
@@ -133,9 +83,9 @@ $(document).ready(function() {
 });
 
 //to validate recaptcha client side
-function validateform(){
+function validateCaptcha(){
 	var captcha_response = grecaptcha.getResponse();
-	return captcha_response.length == 0;
+	return captcha_response.length != 0;
 }
 
 //helper function to hide or show checkmark / x-mark
@@ -153,10 +103,4 @@ function showSuccessDanger(elem, bool){
 		elem.siblings(".is-success").addClass("is-hidden");
 		elem.siblings(".is-danger").removeClass("is-hidden");
 	}
-}
-
-//helper function to validate email address
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
 }
