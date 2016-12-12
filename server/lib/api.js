e00fe4a0c1cd2ce3a2e8a56624ba4c84ea08600f
Wonmin Lee <won2blee@gmail.com>
2016-12-12 16:58:25 -0500

    writing to buffers

--- server/lib/api.js
+++ server/lib/api.js
@@ -94,7 +94,7 @@ function proxyRequest(req, res, address){
 	//edit the body before responding
 	var write = concat(function(response) {
 		if (response != undefined) {
-			console.log(response);
+			response.write("Hello world", "utf-8");
 		}
 		res.end(response);
 	});
