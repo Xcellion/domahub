371cec60129a26a9290bd39850c89a8a4a2b0233
Wonmin Lee <won2blee@gmail.com>
2016-12-12 18:26:29 -0500

    image

--- server/lib/api.js
+++ server/lib/api.js
@@ -105,7 +105,7 @@ function proxyRequest(req, res, address){
 
 	//modify the headers
 	request.get(address).on('response', function (response) {
-		console.log(response.headers['content-type']);
+		console.log(response.headers['content-type'].indexOf("image"));
 		res.writeHead(response.statusCode, response.headers);
 	}).pipe(write);
 }
