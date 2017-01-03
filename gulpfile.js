b05182b038a0094e01dac530f9672ce83f822b53
wonkyu <wonq33@gmail.com>
2017-01-03 17:06:47 -0500

    prelim gulp workflow for sass added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  
+gulp.task('sass', function() {
+  return gulp.src('server/public/sass/**/*.+(scss|sass)')
+    .pipe(sass()) // gulp-sass module - converts sass to css
+    .pipe(gulp.dest('server/public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('server/public/sass/**/*.+(scss|sass)', ['sass']);
 })
