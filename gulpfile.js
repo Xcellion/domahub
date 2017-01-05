1d1d20a80ef7755020b92fdda6fef40104482df6
wonkyu <wonq33@gmail.com>
2017-01-04 21:17:32 -0500

    cleaned up a lot of files and views, added minify-css to gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
   return gulp.src('server/public/sass/**/*.scss')
@@ -8,12 +9,16 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('server/public/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('server/public/sass/**/*.scss', ['sass']);
-})
-
 gulp.task('minify-css', function() {
   return gulp.src('server/public/css/*.css')
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('server/public/css/min'));
-})
+  .pipe(cleanCSS())
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('server/public/css/min'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('server/public/sass/**/*.scss', ['sass']);
+  gulp.watch('server/public/css/*.css', ['minify-css']);
+});
