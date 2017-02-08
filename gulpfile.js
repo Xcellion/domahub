691224f5847635ae64e76f06e887e240026d1c66
wonkyu <wonq33@gmail.com>
2017-02-08 01:30:33 -0500

    fixes to index

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var uncss = require('gulp-uncss');
 
 gulp.task('sass', function() {
   return gulp.src('server/public/sass/**/*.scss')
@@ -11,11 +12,19 @@ gulp.task('sass', function() {
 
 gulp.task('minify-css', function() {
   return gulp.src('server/public/css/*.css')
-  .pipe(cleanCSS())
-  .pipe(rename({
-    suffix: '.min'
-  }))
-  .pipe(gulp.dest('server/public/css/min'));
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('server/public/css/min'));
+});
+
+gulp.task('un-css', function() {
+  return gulp.src('server/public/css/master.css')
+    .pipe(uncss({
+      html: ['server/views/main/*.ejs', 'server/views/templates/footer.ejs', 'server/views/templates/navbar.ejs']
+    }))
+    .pipe(gulp.dest('server/public/css/uncssed'));
 });
 
 gulp.task('watch', function() {
