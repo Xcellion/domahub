2eb87a63fb774ea2ab3d0f8e8bfdfc0a0c989db5
wonkyu <wonq33@gmail.com>
2017-02-10 22:16:19 -0500

    test notification feature for index

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var uncss = require('gulp-uncss');
-var spritesmith = require('gulp.spritesmith');
+// var spritesmith = require('gulp.spritesmith');
 
 gulp.task('sass', function() {
   return gulp.src('server/public/sass/**/*.scss')
@@ -27,16 +27,16 @@ gulp.task('un-css', function() {
     }))
     .pipe(gulp.dest('server/public/css/uncssed'));
 });
-
-gulp.task('sprite', function() {
-  var spriteData = gulp.src('server/public/images/dh-assets/flat-logo/*.png')
-    .pipe(spritesmith({
-      imgName: 'dh-flat-logo-sprite.png',
-      cssName: 'sprite.css'
-    }));
-    spriteData.img.pipe(gulp.dest('server/public/images/dh-assets/flat-logo/'));
-    spriteData.css.pipe(gulp.dest('server/public/images/dh-assets/flat-logo/'));
-});
+//
+// gulp.task('sprite', function() {
+//   var spriteData = gulp.src('server/public/images/dh-assets/flat-logo/*.png')
+//     .pipe(spritesmith({
+//       imgName: 'dh-flat-logo-sprite.png',
+//       cssName: 'sprite.css'
+//     }));
+//     spriteData.img.pipe(gulp.dest('server/public/images/dh-assets/flat-logo/'));
+//     spriteData.css.pipe(gulp.dest('server/public/images/dh-assets/flat-logo/'));
+// });
 
 gulp.task('watch', function() {
   gulp.watch('server/public/sass/**/*.scss', ['sass']);
