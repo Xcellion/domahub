e34ca0a7ed3d0cf4f260a0ba9e0a730123347083
Wonmin Lee <won2blee@gmail.com>
2017-02-14 18:18:46 -0500

    timeout fix

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -204,17 +204,17 @@ module.exports = {
 			delete req.session.mylistings;
 			delete req.session.myrentals;
 			req.logout();
-		}
 
-        //redirect to a listing if we're logging out there
-		if (req.header("Referer")){
-			redirectTo = (req.header("Referer").split("/").indexOf("listing") != -1) ? req.header("Referer") : "/";
+            //redirect to a listing if we're logging out there
+            if (req.header("Referer")){
+                redirectTo = (req.header("Referer").split("/").indexOf("listing") != -1) ? req.header("Referer") : "/";
+            }
+            //or redirect to main page
+            else {
+                redirectTo = "/";
+            }
+            res.redirect(redirectTo);
 		}
-        //or redirect to main page
-		else {
-			redirectTo = "/";
-		}
-		res.redirect(redirectTo);
 	},
 
 	//sign up for a new account
