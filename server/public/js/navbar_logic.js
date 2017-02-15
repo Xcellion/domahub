064858ffd6d31f9b2891c73e4d05eaa7d975b854
wonkyu <wonq33@gmail.com>
2017-02-15 00:42:03 -0500

    changes to mobile nav-menu; still needs work on js

--- server/public/js/navbar_logic.js
+++ server/public/js/navbar_logic.js
@@ -77,7 +77,6 @@ $(document).ready(function() {
 	if (pages_white_nav.indexOf(current_page) != -1){
 		$(".nav-link").removeClass("is-white");
 		$("#nav-logo, .circle-logo").addClass("is-primary").removeClass("is-white");
-		$(".nav-menu").removeClass("is-dark").addClass("is-white");
 		$(".nav-toggle").removeClass("is-white").addClass("is-black");
 		$("#profile-button, #login-modal").addClass("is-primary").removeClass("is-white");
 	}
@@ -102,22 +101,22 @@ function navbarChange(windowelem, pages_white_nav, current_page){
 	if (windowelem.scrollTop() <= 0 && $(".nav").hasClass("has-shadow")) {
 		$(".nav").removeClass("has-shadow is-white");
 		if (pages_white_nav.indexOf(current_page) != -1){
-			$(".nav-link").removeClass("is-white");
+			// $(".nav-link").removeClass("is-white");
 			$("#nav-logo, .circle-logo").addClass("is-primary").removeClass("is-white");
 		}
 		else {
 			$("#profile-button, #login-modal").removeClass("is-primary").addClass("is-white");
 			$("#nav-logo, .circle-logo").removeClass("is-primary").addClass("is-white");
-			$(".nav-link").addClass("is-white");
-			$(".nav-menu").removeClass("is-white").addClass("is-primary");
+			// $(".nav-link").addClass("is-white");
+			// $(".nav-menu").removeClass("is-white").addClass("is-primary");
 			$(".nav-toggle").removeClass("is-black").addClass("is-white");
 		}
 	}
 	//past the top
 	else if (windowelem.scrollTop() > 0 && !$(".nav").hasClass("has-shadow")){
 		$(".nav").addClass("has-shadow is-white");
-		$(".nav-link").removeClass("is-white");
-		$(".nav-menu").removeClass("is-primary").addClass("is-white");
+		// $(".nav-link").removeClass("is-white");
+		// $(".nav-menu").removeClass("is-primary").addClass("is-white");
 		$(".nav-toggle").removeClass("is-white").addClass("is-black");
 		$("#nav-logo, .circle-logo").addClass("is-primary").removeClass("is-white");
 		$("#profile-button, #login-modal").addClass("is-primary").removeClass("is-white");
