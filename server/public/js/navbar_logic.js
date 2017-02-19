1533a9d3090d8eb6bc777cc88008f3a3b7edabfd
wonkyu <wonq33@gmail.com>
2017-02-19 13:24:55 -0500

    pushing new index for test

--- server/public/js/navbar_logic.js
+++ server/public/js/navbar_logic.js
@@ -101,22 +101,22 @@ function navbarChange(windowelem, pages_white_nav, current_page){
 	if (windowelem.scrollTop() <= 0 && $(".nav").hasClass("has-shadow")) {
 		$(".nav").removeClass("has-shadow is-white");
 		if (pages_white_nav.indexOf(current_page) != -1){
-			// $(".nav-link").removeClass("is-white");
+			$(".nav-link").removeClass("is-white");
 			$("#nav-logo, .circle-logo").addClass("is-primary").removeClass("is-white");
 		}
 		else {
 			$("#profile-button, #login-modal").removeClass("is-primary").addClass("is-white");
 			$("#nav-logo, .circle-logo").removeClass("is-primary").addClass("is-white");
-			// $(".nav-link").addClass("is-white");
-			// $(".nav-menu").removeClass("is-white").addClass("is-primary");
+			$(".nav-link").addClass("is-white");
+			$(".nav-menu").removeClass("is-white").addClass("is-black");
 			$(".nav-toggle").removeClass("is-black").addClass("is-white");
 		}
 	}
 	//past the top
 	else if (windowelem.scrollTop() > 0 && !$(".nav").hasClass("has-shadow")){
 		$(".nav").addClass("has-shadow is-white");
-		// $(".nav-link").removeClass("is-white");
-		// $(".nav-menu").removeClass("is-primary").addClass("is-white");
+		$(".nav-link").removeClass("is-white");
+		$(".nav-menu").removeClass("is-black").addClass("is-white");
 		$(".nav-toggle").removeClass("is-white").addClass("is-black");
 		$("#nav-logo, .circle-logo").addClass("is-primary").removeClass("is-white");
 		$("#profile-button, #login-modal").addClass("is-primary").removeClass("is-white");
