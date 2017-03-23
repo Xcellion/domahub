9b47ec4baad1edd88e1e1e5717b3d8eb6ec39cc4
Wonmin Lee <won2blee@gmail.com>
2017-03-23 13:50:45 -0400

    wtf

--- server/server.js
+++ server/server.js
@@ -119,6 +119,8 @@ app.get('*', function(req, res){
 	res.redirect('/nothinghere');
 });
 
+console.log('fjkdasljfsajklfjasldfjsafjklsajflasjkldfsjklfjlkasjfklsajfklasdjlfkdjaskldfjksl');
+
 // //HTTP website on port 8080
 // serverHTTP(app).listen(8080, function(){
 // 	console.log("HTTP website listening on port 8080");
