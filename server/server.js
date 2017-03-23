fb34ae5089d898667f6daefb072c67cfadce932e
Wonmin Lee <won2blee@gmail.com>
2017-03-23 13:48:32 -0400

    chaning port seeing what happens

--- server/server.js
+++ server/server.js
@@ -120,6 +120,6 @@ app.get('*', function(req, res){
 });
 
 //HTTP website on port 8080
-serverHTTP(app).listen(8080, function(){
+serverHTTP(app).listen(9090, function(){
 	console.log("HTTP website listening on port 8080");
 });
