56e1bcc574a01d876a0f76a86bbc33253f503534
Wonmin Lee <won2blee@gmail.com>
2017-03-23 12:04:39 -0400

    insecure page causes login issue

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -163,7 +163,6 @@ module.exports = {
 	checkLoggedIn : function(req, res, next) {
         console.log("F: Checking if authenticated...");
 		//if user is authenticated in the session, carry on
-        console.log(req.session, req.isAuthenticated());
 		if (req.isAuthenticated()){
 			res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
 			res.header('Expires', '-1');
