fff24ca459f79d31151545f9856305bc5b36f2a5
Wonmin Lee <won2blee@gmail.com>
2017-03-25 18:11:30 -0400

    alexa dev and headlinks certify js

--- server/routes/dev_routes.js
+++ server/routes/dev_routes.js
@@ -9,15 +9,40 @@ var dns = require("dns");
 
 var randomstring = require("randomstring");
 
+var awis = require('awis');
+
 module.exports = function(app, db, auth, error){
     Auth = auth;
     Account = new account_model(db);
     Listing = new listing_model(db);
 
     app.get("/dnsrecords/:domain_name", dnsRecords);
+    app.get("/alexa/:domain_name", alexa);
     app.get("/createcodes/:number", [
         createSignupCodes
     ]);
+}
+
+//testing alexa get
+function alexa(req, res, next){
+    if (req.params.domain_name){
+        var client = awis({
+            key: "AKIAJVS3NZJHFWJ7QKKA",
+            secret: "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"
+        });
+
+        client({
+            'Action': 'UrlInfo',
+            'Url': req.params.domain_name,
+            'ResponseGroup': 'Related,TrafficData,ContentData'
+        }, function (err, data) {
+            if(err) { console.log(err); return; }
+            res.send(data);
+        });
+    }
+    else {
+        res.send("you need a domain name");
+    }
 }
 
 //testing dns records look up
