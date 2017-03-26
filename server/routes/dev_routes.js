316806836e12e03ae16d3240e8dcbb2f87a2fab3
Wonmin Lee <won2blee@gmail.com>
2017-03-25 20:52:59 -0400

    still verified error

--- server/routes/dev_routes.js
+++ server/routes/dev_routes.js
@@ -26,10 +26,16 @@ module.exports = function(app, db, auth, error){
 //testing alexa get
 function alexa(req, res, next){
     if (req.params.domain_name){
+        console.log("F: Getting Alexa traffic information for " + req.params.domain_name);
         var client = awis({
             key: "AKIAJVS3NZJHFWJ7QKKA",
             secret: "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"
         });
+
+// UrlInfo - get information about pages and sites on the web - their traffic, content, and related sites
+// TrafficHistory - get a history of traffic rank
+// CategoryBrowse, CategoryListings - get lists of sites within a specific category ordered by traffic rank, or create a browseable directory of websites
+// SitesLinkingIn - get a list of sites linking in to a specified site
 
         client({
             'Action': 'UrlInfo',
