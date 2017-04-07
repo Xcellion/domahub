cd8a8476869487fe3f3a69f94c5c12091370b4d0
Wonmin Lee <won2blee@gmail.com>
2017-04-06 22:37:36 -0400

    checkout pretty

--- server/public/js/routes/listings/listing_checkout_logic.js
+++ server/public/js/routes/listings/listing_checkout_logic.js
@@ -72,49 +72,75 @@ $(document).ready(function () {
     //--------------------------------------------------------------------------------------------------------- CHOICE BLOCKS
 
     //click choice block
+    var which_address = "address-regular-message";
     $(".choice-block").on("click", function() {
-        $("#choices-block").addClass("is-hidden");
-        $("#choices-selected").removeClass("is-hidden");
-        $(".back-button").removeClass("is-hidden");
+        var which_choice = $(this);
+        $("#choices-block").stop().fadeOut(250, function(){
+            $("#choices-selected").stop().fadeIn(250);
 
-        //build a website
-        if ($(this).hasClass("build-choice")) {
-            $(".build-choice-column").toggleClass("is-hidden");
-<<<<<<< HEAD
-            showMessage("address-build-message");
-=======
-            $("#checkout-msg1").text("We are NOT sponsored by any of these providers below. Clicking the image will send you to the respective website.");
->>>>>>> cb2a4d2011ad55175e101d3b506e2ad96a61020b
+            //build a website
+            if (which_choice.hasClass("build-choice")) {
+                $(".build-choice-column").removeClass("is-hidden");
+                showMessage("address-build-message");
+                which_address = "address-build-message";
+            }
+
+            //link a website
+            else if (which_choice.hasClass("link-choice")) {
+                $(".link-choice-column").removeClass("is-hidden");
+                showMessage("address-link-message");
+                which_address = "address-link-message";
+            }
+
+            //forward to a website
+            else {
+                $(".forward-choice-column").removeClass("is-hidden");
+                showMessage("address-forward-message");
+                which_address = "address-forward-message";
+            }
+        });
+
+    });
+
+    //back button on address selection
+    $(".back-button").on("click", function() {
+        $("#choices-selected").stop().fadeOut(250, function(){
+            $("#choices-block").stop().fadeIn(250);
+            $(".choice-column").addClass('is-hidden');
+            showMessage("address-regular-message");
+            $(".address-input").val("");
+            $("#rental-will-msg").addClass('is-hidden');
+            $("#rental-will-duration-msg").addClass('is-hidden');
+        });
+    });
+
+    //change the card to display what will happen
+    $(".address-input").on("change keyup paste", function(){
+        if ($(this).val() != ""){
+            var value_clipped = ($(this).val().length > 35) ? $(this).val().substr(0, 35) + "..." : $(this).val();
+            if ($(this).attr("id") == "address-forward-input"){
+                var rental_type_text = "will forward to <a href=" + $(this).val() + " class='is-accent'>this link </br>(" + value_clipped + ")</a>";
+            }
+            else {
+                var rental_type_text = "will display the content on <a href=" + $(this).val() + " class='is-accent'>this link </br>(" + value_clipped + ")</a>";
+            }
+            $("#rental-will-msg").html(rental_type_text).removeClass('is-hidden');
+            $("#rental-will-duration-msg").removeClass('is-hidden');
         }
-
-        //link a website
-        else if ($(this).hasClass("link-choice")) {
-            $(".link-choice-column").toggleClass("is-hidden");
-<<<<<<< HEAD
-            showMessage("address-link-message");
-=======
-            $("#checkout-msg1").text("You may link anything on the web to your rental (e.g. an image, a gif, a website). Enter the URL below and we will display it on www." + listing_info.domain_name);
->>>>>>> cb2a4d2011ad55175e101d3b506e2ad96a61020b
-        }
-
-        //forward to a website
         else {
-            $(".forward-choice-column").toggleClass("is-hidden");
-<<<<<<< HEAD
-            showMessage("address-forward-message");
-=======
-            $("#checkout-msg1").text("We can forward your domain rental to another existing website. Just enter the URL below!");
->>>>>>> cb2a4d2011ad55175e101d3b506e2ad96a61020b
+            $("#rental-will-msg").addClass('is-hidden');
+            $("#rental-will-duration-msg").addClass('is-hidden');
         }
     });
 
-    //back button on
-    $(".back-button").on("click", function() {
-        $(this).addClass("is-hidden");
-        showMessage("address-regular-message");
-        $("#choices-block").removeClass("is-hidden");
-        $("#choices-selected").addClass("is-hidden");
-        $(".choice-column").addClass("is-hidden");
+    //submit the new address
+    $(".address-next-button").on('click', function(){
+        $("#step-header-site").addClass('is-disabled');
+        $("#step-content-site").addClass('is-hidden');
+
+        $("#step-header-payment").removeClass('is-disabled');
+        $("#step-content-payment").removeClass('is-hidden');
+        showMessage("stripe-regular-message");
     });
 
     //--------------------------------------------------------------------------------------------------------- PAYMENT
@@ -175,6 +201,16 @@ $(document).ready(function () {
     //checkout button
     $('#checkout-button').on("click", function(e){
         submitStripe($(this));
+    });
+
+    //go back to address
+    $("#back-to-address-button").on('click', function(){
+        $("#step-header-payment").addClass('is-disabled');
+        $("#step-content-payment").addClass('is-hidden');
+
+        $("#step-header-site").removeClass('is-disabled');
+        $("#step-content-site").removeClass('is-hidden');
+        showMessage(which_address);
     });
 
 });
