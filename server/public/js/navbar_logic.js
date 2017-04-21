5d31767261baa4d3fabb0e8661fb6a0a0dc2d9db
Wonmin Lee <won2blee@gmail.com>
2017-04-21 09:14:01 -0400

    fixed navbar

--- server/public/js/navbar_logic.js
+++ server/public/js/navbar_logic.js
@@ -1,80 +1,86 @@
 $(document).ready(function() {
-	//used to display the message sent from the server
-	if (typeof message != "undefined" && message){
-		$("#message").text(message);
-		if (message == "Invalid username / password!" || message == "Login error!"){
-			$('#modal-login').addClass('is-active');
-			$("#message").css("color", "#ed1c24");
-		}
-	}
+	//only if navbar is there
+	if ($("#navbar").length > 0){
 
-	//redirect search to the appropriate /listing page
-	$("#searchbar-form").submit(function(e){
-		e.preventDefault();
-		var domain_name = $('#domain-name').val();
-
-		//default to .com if theres nothing provided
-		if (domain_name.indexOf(".") == -1){
-			domain_name += ".com";
-			$('#domain-name').val(domain_name);
-		}
-
-        window.location = window.location.origin + "/listing/" + domain_name;
-	});
-
-	// adds is-active to login button
-	$('.login-modal').click(function() {
-	  $('#modal-login').addClass('is-active');
-	});
-
-	// various ways to close login dropdown menu
-	$('.modal-close, .modal-background').click(function() {
-	  $('#modal-login').removeClass('is-active');
-	  $("#message").attr("style", "");
-	});
-
-	$(document).keyup(function(e) {
-		if (e.which == 27) {
-			$('#modal-login').removeClass('is-active');
-		}
-	});
-
-	//close modal if user is logged in
-	if (user){
-		$("#login_modal").removeAttr("style");
-	}
-
-	//hamburger toggle button
-	$(".nav-toggle").click(function() {
-	  $(this).toggleClass("is-active");
-	  $(".nav-menu").toggleClass("is-active");
-	});
-
-	//profile dropdown logic
-	$(document).on("click", function(event) {
-		//clicked off profile dropdown
-		if (!$(event.target).closest("#profile-button").length && !$("#profile-dropdown").hasClass("is-hidden")) {
-			if (!$(".nav").hasClass("is-white")){
-				$("#profile-button").toggleClass("is-outlined");
+		//used to display the message sent from the server
+		if (typeof message != "undefined" && message){
+			$("#message").text(message);
+			if (message == "Invalid username / password!" || message == "Login error!"){
+				$('#modal-login').addClass('is-active');
+				$("#message").css("color", "#ed1c24");
 			}
-			$("#profile-dropdown").addClass("is-hidden");
-			$("#profile-button").toggleClass("is-active");
 		}
-		//clicked on profile dropdown
-		else if ($(event.target).closest("#profile-button").length){
-			if (!$(".nav").hasClass("is-white")){
-				$("#profile-button").toggleClass("is-outlined");
+
+		//redirect search to the appropriate /listing page
+		$("#searchbar-form").submit(function(e){
+			e.preventDefault();
+			var domain_name = $('#domain-name').val();
+
+			//default to .com if theres nothing provided
+			if (domain_name.indexOf(".") == -1){
+				domain_name += ".com";
+				$('#domain-name').val(domain_name);
 			}
-			$("#profile-button").toggleClass("is-active");
-			$("#profile-dropdown").toggleClass("is-hidden");
+
+			window.location = window.location.origin + "/listing/" + domain_name;
+		});
+
+		// adds is-active to login button
+		$('.login-modal').click(function() {
+		  $('#modal-login').addClass('is-active');
+		});
+
+		// various ways to close login dropdown menu
+		$('.modal-close, .modal-background').click(function() {
+		  $('#modal-login').removeClass('is-active');
+		  $("#message").attr("style", "");
+		});
+
+		$(document).keyup(function(e) {
+			if (e.which == 27) {
+				$('#modal-login').removeClass('is-active');
+			}
+		});
+
+		//close modal if user is logged in
+		if (user){
+			$("#login_modal").removeAttr("style");
 		}
-	});
 
-	//list of pages that need a green doma logo // white background. otherwise transparent and white logo
-	var pages_white_nav = ["checkout", "listings", "faq", "contact", "profile", "mission", "about", "press", "careers", "terms", "privacy", "nothinghere"];
-	var current_page = (window.location.pathname == "/") ? "/" : window.location.pathname.split("/").pop();
+		//hamburger toggle button
+		$(".nav-toggle").click(function() {
+		  $(this).toggleClass("is-active");
+		  $(".nav-menu").toggleClass("is-active");
+		});
+		
+		//profile dropdown logic
+		$(document).on("click", function(event) {
+			//clicked off profile dropdown
+			if (!$(event.target).closest("#profile-button").length && !$("#profile-dropdown").hasClass("is-hidden")) {
+				if (!$(".nav").hasClass("is-white")){
+					$("#profile-button").toggleClass("is-outlined");
+				}
+				$("#profile-dropdown").addClass("is-hidden");
+				$("#profile-button").toggleClass("is-active");
+			}
+			//clicked on profile dropdown
+			else if ($(event.target).closest("#profile-button").length){
+				if (!$(".nav").hasClass("is-white")){
+					$("#profile-button").toggleClass("is-outlined");
+				}
+				$("#profile-button").toggleClass("is-active");
+				$("#profile-dropdown").toggleClass("is-hidden");
+			}
+		});
 
-	if ($("#navbar").length){
+		//list of pages that need a green doma logo // white background. otherwise transparent and white logo
+		var pages_white_nav = ["checkout", "listings", "faq", "contact", "profile", "mission", "about", "press", "careers", "terms", "privacy", "nothinghere"];
+		var current_page = (window.location.pathname == "/") ? "/" : window.location.pathname.split("/")[1];
+
+		if (window.location.pathname.split("/").pop == "checkout"){
+			current_page = "checkout";
+		}
+
 		//remove white link if not a page that needs it
 		if (pages_white_nav.indexOf(current_page) != -1){
 			$(".nav-link").removeClass("is-white");
