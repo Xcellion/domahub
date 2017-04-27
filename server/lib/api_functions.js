b5261d495d54f2ad25fbde47920e3d73e79a3df6
Wonmin Lee <won2blee@gmail.com>
2017-04-26 22:21:48 -0400

    api PATH check

--- server/lib/api_functions.js
+++ server/lib/api_functions.js
@@ -52,7 +52,7 @@ function checkHost(req, res, next){
 //send the current rental details and information for a listing
 function getCurrentRental(req, res, domain_name, path){
 	//requesting something besides main page, pipe the request
-	if (req.session.rented_info){
+	if (req.session.rented_info.path == path){
 		console.log("F: Proxying rental request for an existing session for " + domain_name + "!");
 		searchAndDirect(req.session.rented_info, req, res);
 	}
