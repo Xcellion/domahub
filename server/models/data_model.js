4bfd796caee83ec3d37e0047e1df0538391a08a6
Wonmin Lee <won2blee@gmail.com>
2017-05-01 21:49:15 -0400

    tracking check availability now

--- server/models/data_model.js
+++ server/models/data_model.js
@@ -55,27 +55,35 @@ data_model.prototype.newRentalHistory = function(history_info, callback){
 	data_query(query, "Failed to add rental view history for rental #" + history_info.rental_id + "!", callback, history_info);
 }
 
-//creates new rental times for unavailable listings
-//BULK INSERT NEEDS TRIPLE NESTED ARRAYS
-data_model.prototype.newDesiredRentalTimes = function(domain_name, desired_times_info, callback){
-	console.log("DB: Attempting to create new desired times for domain" + domain_name + "...");
-	query = "INSERT INTO stats_desired_times (domain_name, timestamp, start_date, duration, account_id, user_ip) VALUES ? ";
-	data_query(query, "Failed to add new desired times for domain" + domain_name + "!", callback, [desired_times_info]);
+//creates a new entry for a checked availability data row
+data_model.prototype.newCheckAvailHistory = function(history_info, callback){
+	console.log("DB: Adding new availability check history item for domain: " + history_info.domain_name + "...");
+	query = "INSERT INTO stats_availcheck_history \
+			SET ? "
+	data_query(query, "Failed to add availability check history item for domain:" + history_info.domain_name + "!", callback, history_info);
 }
 
-//gets the maximum and minimum prices for all domains
-data_model.prototype.getMinMaxPrices = function(callback){
-	console.log("DB: Attempting to get maximum and minimum prices for all domains...");
-	query = "SELECT \
-				MIN(hour_price) AS min_hour_price, \
-				MAX(hour_price) AS max_hour_price, \
-				MIN(day_price) AS min_day_price, \
-				MAX(day_price) AS max_day_price, \
-				MIN(week_price) AS min_week_price, \
-				MAX(week_price) AS max_week_price, \
-				MIN(month_price) AS min_month_price, \
-				MAX(month_price) AS max_month_price \
-			FROM `listings` \
-			WHERE listings.status >= 1";
-	data_query(query, "Failed to get maxmimum and minimum prices for all domains!", callback);
-}
+// //creates new rental times for unavailable listings
+// //BULK INSERT NEEDS TRIPLE NESTED ARRAYS
+// data_model.prototype.newDesiredRentalTimes = function(domain_name, desired_times_info, callback){
+// 	console.log("DB: Attempting to create new desired times for domain" + domain_name + "...");
+// 	query = "INSERT INTO stats_desired_times (domain_name, timestamp, start_date, duration, account_id, user_ip) VALUES ? ";
+// 	data_query(query, "Failed to add new desired times for domain" + domain_name + "!", callback, [desired_times_info]);
+// }
+//
+// //gets the maximum and minimum prices for all domains
+// data_model.prototype.getMinMaxPrices = function(callback){
+// 	console.log("DB: Attempting to get maximum and minimum prices for all domains...");
+// 	query = "SELECT \
+// 				MIN(hour_price) AS min_hour_price, \
+// 				MAX(hour_price) AS max_hour_price, \
+// 				MIN(day_price) AS min_day_price, \
+// 				MAX(day_price) AS max_day_price, \
+// 				MIN(week_price) AS min_week_price, \
+// 				MAX(week_price) AS max_week_price, \
+// 				MIN(month_price) AS min_month_price, \
+// 				MAX(month_price) AS max_month_price \
+// 			FROM `listings` \
+// 			WHERE listings.status >= 1";
+// 	data_query(query, "Failed to get maxmimum and minimum prices for all domains!", callback);
+// }
