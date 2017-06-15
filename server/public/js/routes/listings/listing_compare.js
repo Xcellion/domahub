f17a4969da5a8881cfda49ebf1f7c042c2625ea9
Wonmin Lee <won2blee@gmail.com>
2017-06-15 01:24:00 -0400

    compare starting
    Former-commit-id: 8334e6f3a1d8a27bf4090dbf15694751d032e91c

---
+++ server/public/js/routes/listings/listing_compare.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	//<editor-fold>-----------------------------------------------------------------------------------COMPARE
+
+	if (compare){
+		console.log("Compare mode activated");
+	}
+
+	//</editor-fold>
+
+});
--- server/public/js/routes/listings/listing_listed_logic.js
+++ server/public/js/routes/listings/listing_listed_logic.js
@@ -663,15 +663,4 @@ function anyFreeDayOverlap(starttime, endtime){
 	}
 }
 
-//get query string from URL
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
 //</editor-fold>
