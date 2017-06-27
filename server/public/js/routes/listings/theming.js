a054643c8046ea2810b827973ee5b706fc68ab2f
Wonmin Lee <won2blee@gmail.com>
2017-06-26 22:58:51 -0400

    main page links to examples
    Former-commit-id: c4bed02464668157667d73d39fa2338ccd6202a8

--- server/public/js/routes/listings/theming.js
+++ server/public/js/routes/listings/theming.js
@@ -1,39 +1,41 @@
 $(document).ready(function() {
+  //remove class to prevent screen flash DH green
+  $("#compare-preview").removeClass('is-hidden');
 
-    //remove class to prevent screen flash DH green
-    $("#compare-preview").removeClass('is-hidden');
+  //if it's premium, check if theres any customization in the design
+  if (listing_info.premium){
+    setupCustomColors();
 
-    if (listing_info.premium){
-        setupCustomColors();
+    stylize(listing_info.secondary_color, "#typed-slash", "color");
 
-        stylize(listing_info.secondary_color, "#typed-slash", "color");
+    if (listing_info.background_image){
+      $("#compare-preview").css("background", "url(" + listing_info.background_image + ") center/cover no-repeat").on("error", function(){
+        $(this).css({
+          background: "",
+          "background-color": "#FFF"
+        });
+      });
+    }
 
-        if (listing_info.background_image){
-            $("#compare-preview").css("background", "url(" + listing_info.background_image + ") center/cover no-repeat").on("error", function(){
-                $(this).css({
-                    background: "",
-                    "background-color": "#FFF"
-                });
-            });
-        }
+    if (listing_info.logo){
+      //hide dh logo
+      $("#powered-by").html('&nbsp;');
+      $("#dh_logo").addClass('is-hidden');
 
-        if (listing_info.logo){
-            //hide dh logo
-            $("#powered-by").html('&nbsp;');
-            $("#dh_logo").addClass('is-hidden');
-
-            //show custom logo
-            $("#custom_logo").attr("src", listing_info.logo).on("error", function(){
-                console.log("Error loading logo!");
-            });
-        }
-        else {
-            $(".footer").addClass('is-hidden');
-        }
+      //show custom logo
+      $("#custom_logo").attr("src", listing_info.logo).on("error", function(){
+        console.log("Error loading logo!");
+      });
     }
     else {
-        setupDefaultColors();
+      $(".footer").addClass('is-hidden');
     }
+  }
+
+  //if not using the compare module, use default DH styling
+  else if (!compare){
+    setupDefaultColors();
+  }
 });
 
 var primaryColor = "#3CBC8D";
@@ -52,47 +54,47 @@ function calculateLuminance(rgb) {
   if (hexValue.length !== 6) {
     return 0;
   }
-    r = parseInt(hexValue.substring(0,2), 16) / 255;
-    g = parseInt(hexValue.substring(2,4), 16) / 255;
-    b = parseInt(hexValue.substring(4,6), 16) / 255;
+  r = parseInt(hexValue.substring(0,2), 16) / 255;
+  g = parseInt(hexValue.substring(2,4), 16) / 255;
+  b = parseInt(hexValue.substring(4,6), 16) / 255;
 
-    // calculate the overall luminance of the color
-    var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  // calculate the overall luminance of the color
+  var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-    if (luminance > 0.8) {
-        return "#222";
-    }
-    else {
-        return "#fff";
-    }
+  if (luminance > 0.8) {
+    return "#222";
+  }
+  else {
+    return "#fff";
+  }
 }
 
 function stylize(color, element, style) {
-    if (color){
-        $(element).css(style, color);
-        if (style == "background-color") {
-            $(element).css("color", calculateLuminance(color));
-        }
+  if (color){
+    $(element).css(style, color);
+    if (style == "background-color") {
+      $(element).css("color", calculateLuminance(color));
     }
+  }
 }
 
 //function to setup any custom premium colors
 function setupCustomColors(){
-    stylize(listing_info.primary_color, ".is-primary", "color");
-    stylize(listing_info.primary_color, ".daterangepicker td.active, .daterangepicker td.active:hover", "background-color");
-    stylize(listing_info.primary_color, ".button", "background-color");
-    stylize(listing_info.primary_color, ".tag", "background-color");
-    stylize(listing_info.font_color, ".regular-font", "color");
-    stylize(listing_info.primary_color, "h1,h2,h3", "color");
-    stylize(listing_info.tertiary_color, ".is-info", "color");
-    stylize(listing_info.secondary_color, ".is-accent", "color");
-    stylize(listing_info.secondary_color, ".is-accent.button", "background-color");
-    stylize(listing_info.background_color, "#compare-preview", "background-color");
-    stylize(listing_info.font_name, "#domain-title", "font-family");
-    stylize(listing_info.font_name, "#typed-slash", "font-family");
+  stylize(listing_info.primary_color, ".is-primary", "color");
+  stylize(listing_info.primary_color, ".daterangepicker td.active, .daterangepicker td.active:hover", "background-color");
+  stylize(listing_info.primary_color, ".button", "background-color");
+  stylize(listing_info.primary_color, ".tag", "background-color");
+  stylize(listing_info.font_color, ".regular-font", "color");
+  stylize(listing_info.primary_color, "h1,h2,h3", "color");
+  stylize(listing_info.tertiary_color, ".is-info", "color");
+  stylize(listing_info.secondary_color, ".is-accent", "color");
+  stylize(listing_info.secondary_color, ".is-accent.button", "background-color");
+  stylize(listing_info.background_color, "#compare-preview", "background-color");
+  stylize(listing_info.font_name, "#domain-title", "font-family");
+  stylize(listing_info.font_name, "#typed-slash", "font-family");
 }
 
 //set up default DH colors
 function setupDefaultColors(){
-    stylize(primaryColor, ".tag", "background-color");
+  stylize(primaryColor, ".tag", "background-color");
 }
