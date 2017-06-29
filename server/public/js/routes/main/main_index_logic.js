575e439727975989f19f14ea4e95b0958ccac26a
Wonmin Lee <won2blee@gmail.com>
2017-06-29 19:55:17 -0400

    fixed anchor tag not working from outside the page
    Former-commit-id: 096c3284ef034b9535a1d6a62ea43689b9fe7275

--- server/public/js/routes/main/main_index_logic.js
+++ server/public/js/routes/main/main_index_logic.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+  //focus the try it input if there wasnt a anchor tag in the URL
+  if (!window.location.hash){
+    $("#domain-name-input").focus();
+  }
+
   //scroll down on learn more
   $("#learn-more-wrapper").on("click", function(e){
     e.preventDefault();
