e0dabbbc082fc8c8f96293dfb528d6e4a52f896a
Wonmin Lee <won2blee@gmail.com>
2017-07-06 18:49:45 -0400

    http redirect is broken
    Former-commit-id: 796fe6a5ee1a08ab511ce46f5b08eb9bb7e629ac

--- server/letsencrypt_server.js
+++ server/letsencrypt_server.js
@@ -6,47 +6,35 @@ db.connect();  //connect to the database
 
 // returns an instance of node-greenlock with additional helper methods
 var lex = require('greenlock-express').create({
-  // server: 'staging'
-  server: 'https://acme-v01.api.letsencrypt.org/directory'
-
-// If you wish to replace the default plugins, you may do so here
-, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
-, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
-
-// You probably wouldn't need to replace the default sni handler
-// See https://git.daplie.com/Daplie/le-sni-auto if you think you do
-//, sni: require('le-sni-auto').create({})
-
-, approveDomains: approveDomains
+  server: 'staging',
+  // server: 'https://acme-v01.api.letsencrypt.org/directory',
+  approveDomains: approveDomains
 });
 
 function approveDomains(opts, certs, cb) {
   // This is where you check your database and associated
   // email addresses with domains and agreements and such
 
-  opts.domains = ["w3bbi.com"];
-
   // The domains being approved for the first time are listed in opts.domains
   // Certs being renewed are listed in certs.altnames
   if (certs) {
     console.log(certs);
-    opts.domains = certs.altnames;
+    opts.domains = ["w3bbi.com"];
   }
   else {
     opts.email = 'wonmin@w3bbi.com';
     opts.agreeTos = true;
   }
 
-  // NOTE: you can also change other options such as `challengeType` and `challenge`
-  // opts.challengeType = 'http-01';
-  // opts.challenge = require('le-challenge-fs').create({});
-
-  console.log(opts, certs);
   cb(null, { options: opts, certs: certs });
 }
 
 // handles acme-challenge and redirects to https
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(8080, function () {
+require('http').createServer(lex.middleware(require('redirect-https')({
+  port: 4343,
+  trustProxy: true,
+  body: "FUCK u"
+}))).listen(8080, function () {
   console.log("Listening for ACME http-01 challenges on", this.address());
 });
 
