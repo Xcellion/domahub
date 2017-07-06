ca5abecfc3c97f1362f514b3a6b917005b9c145e
Wonmin Lee <won2blee@gmail.com>
2017-07-06 18:38:43 -0400

    changing to prod in staging variable
    Former-commit-id: d4950d22f09ef11adab1478d4201b2670bd34a22

--- server/letsencrypt_server.js
+++ server/letsencrypt_server.js
@@ -6,11 +6,10 @@ db.connect();  //connect to the database
 
 // returns an instance of node-greenlock with additional helper methods
 var lex = require('greenlock-express').create({
-  // set to https://acme-v01.api.letsencrypt.org/directory in production
-  server: 'staging'
+  // server: 'staging'
+  server: 'https://acme-v01.api.letsencrypt.org/directory'
 
 // If you wish to replace the default plugins, you may do so here
-//
 , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
 , store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
 
