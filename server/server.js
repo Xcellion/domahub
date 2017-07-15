cf67192f91c004a2e86ccdf1c2bc1c132587abe8
Wonmin Lee <won2blee@gmail.com>
2017-07-14 20:45:59 -0400

    server.js was set at port 80, changing to 8080
    Former-commit-id: dcddd1cdd24993b1c5c7a9f54b021a731431dd87

--- server/server.js
+++ server/server.js
@@ -136,6 +136,6 @@ app.get('*', function(req, res){
 });
 
 //HTTP website on port 8080
-serverHTTP(app).listen(80, function(){
+serverHTTP(app).listen(8080, function(){
   console.log("HTTP website listening on port 8080");
 });
