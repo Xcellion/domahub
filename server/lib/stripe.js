71692df5232e983398a8ce0f3409c9375c35ed36
Wonmin Lee <won2blee@gmail.com>
2017-07-14 20:57:49 -0400

    something wrong with LE Api functions
    Former-commit-id: 1c1276a3a9199b58c8e2eed09bf326c96ff82f5c

--- server/lib/stripe.js
+++ server/lib/stripe.js
@@ -561,7 +561,7 @@ module.exports = {
   checkStripeSubscription : function(req, res, next){
     var domain_name = (req.session.api_domain) ? req.session.api_domain : req.params.domain_name;
     var listing_info = (req.session.listing_info) ? req.session.listing_info : getUserListingObj(req.user.listings, domain_name);
-
+    console.log(listing_info);
     //if subscription id exists in our database
     if (listing_info && listing_info.stripe_subscription_id){
       console.log("SF: Checking if Stripe subscription for listing is still active...");
