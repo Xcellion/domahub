a028e4036a40d1c9656447cbc2a13fbeb1296eba
Wonmin Lee <won2blee@gmail.com>
2017-08-24 19:17:53 -0400

    removed js_scripts, fixed forgot,reset,verify pages
    Former-commit-id: 3fda0be63f93c38d7586a16f7b29b0a8faa1b10e

--- server/public/js/routes/account/account_pw_forgot_logic.js
+++ server/public/js/routes/account/account_pw_forgot_logic.js
@@ -1,36 +1,26 @@
 $(document).ready(function() {
-
-  $('#target').submit(function(event){
+  $('#forgot-form').submit(function(event){
     event.preventDefault();
+    $("#forgot-button").addClass('is-loading');
+    $.ajax({
+      type: "POST",
+      url: "/forgot",
+      data: {
+        email: $("#email").val()
+      }
+    }).done(function(data){
+      //reset the data
+      $("#email").val("");
 
-    //if no email is entered
-    if (!$("#email").val()) {
-      $("#message").fadeOut(100, function(){
-        $("#message").css("color", "#ed1c24").html("Please enter your email address!").fadeIn(100);
-      });
-      return false;
-    }
-    else {
-      $.ajax({
-        type: "POST",
-        url: "/forgot",
-        data: {
-          email: $("#email").val()
-        }
-      }).done(function(data){
-        //reset the data
-        $("#email").val("");
-
-        if (data.state == "success"){
-          $("#message").text("Success! Please check your email for further instructions");
-          $("#form_to_hide").hide();
-          $("#accept").show();
-        }
-        else {
-          $("#login-button").removeClass('is-loading');
-          $("#message").text(data.message);
-        }
-      });
-    }
+      if (data.state == "success"){
+        $("#message").text("Success! Please check your email for further instructions.");
+        $("#form_to_hide").hide();
+        $("#accept").show();
+      }
+      else {
+        $("#forgot-button").removeClass('is-loading');
+        $("#message").text(data.message);
+      }
+    });
   });
 });
