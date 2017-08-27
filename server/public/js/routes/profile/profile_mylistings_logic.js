333fff96816bc2b117f677554a7cbd0eec3a5098
Wonmin Lee <won2blee@gmail.com>
2017-08-27 18:10:25 -0400

    refresh offers button
    Former-commit-id: 8e39ef13ea3c11abbbb550da0f5bde6fce6d598b

--- server/public/js/routes/profile/profile_mylistings_logic.js
+++ server/public/js/routes/profile/profile_mylistings_logic.js
@@ -522,7 +522,7 @@ function editRowPurchased(listing_info){
   //</editor-fold>
 
   //<editor-fold>-------------------------------DESIGN TAB EDITS-------------------------------
-  
+
   //function to switch theme
   function switchTheme(theme_name){
     var theme_to_load = findTheme(theme_name);
@@ -679,6 +679,7 @@ function editRowPurchased(listing_info){
   function showLoadingOffers(){
     $("#loading-offers").removeClass('is-hidden');
     $(".hidden-while-loading-offers").addClass('is-hidden');
+    $("#offers-wrapper").empty();
   }
 
   //function to get offers on a domain
@@ -694,6 +695,8 @@ function editRowPurchased(listing_info){
         //update local listings variable
         if (current_listing){
           (function(current_listing){
+
+            $("#refresh-offers").removeClass('is-loading');
 
             //update the change row handler
             $("#row-listing_id" + current_listing.id).off().on("click", function(e){
@@ -712,14 +715,12 @@ function editRowPurchased(listing_info){
     //show offers if we have it
     if (listing_info.offers == undefined){
       showLoadingOffers();
-      $("#offers-wrapper").empty();
     }
     //hide loading msg
     else {
       $("#loading-offers").addClass('is-hidden');
-      $("#offers-wrapper").empty();
 
-      //show rejected offers button
+      //rejected offers button
       $("#show-rejected-offers").removeClass('is-primary').off().on('click', function(){
         $(".rejected-offer").toggleClass('is-hidden');
         $(this).toggleClass('is-primary').find(".fa").toggleClass('fa-toggle-on fa-toggle-off');
@@ -729,6 +730,12 @@ function editRowPurchased(listing_info){
           $("#no-offers").toggleClass('is-hidden');
         }
       }).find(".fa").removeClass('fa-toggle-on').addClass('fa-toggle-off');
+
+      //refresh offers button
+      $("#refresh-offers").off().on('click', function(){
+        $(this).addClass('is-loading');
+        getDomainOffers(listing_info.domain_name);
+      });
 
       //sort offers
       $("#offers-sort-select").val("timestamp_desc").off().on("change", function(){
