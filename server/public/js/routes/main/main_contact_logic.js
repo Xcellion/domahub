3fb4ccb03e792606c5ce1bcd8be71c8a7e1d461c
Wonmin Lee <won2blee@gmail.com>
2017-09-01 16:25:55 -0400

    fixed contact form error message
    Former-commit-id: 98898d6ac40c86bb7d350f99c2e2e9b7685a7798

--- server/public/js/routes/main/main_contact_logic.js
+++ server/public/js/routes/main/main_contact_logic.js
@@ -1,48 +1,45 @@
-var can_submit = true;
-
 $(document).ready(function() {
 
-    $("#contact-form").submit(function(e){
-        e.preventDefault();
-        submitContact();
-    });
+  $("#contact-form").on("submit", function(e){
+    e.preventDefault();
+    submitContact();
+  });
 
-    //hide any existing error messages
-    $("input, textarea").on("keyup", function(e){
-        $("#error_message").addClass("is-hidden");
-    });
+  //hide any existing error messages
+  $("input, textarea").on("keyup", function(e){
+    $("#error_message").addClass("is-hidden");
+  });
 
-    //random contact characters
-    var random_char = random_characters[Math.floor(Math.random()*random_characters.length)];
-    $("#contact_name").attr("placeholder", random_char.name);
-    $("#contact_email").attr("placeholder", random_char.email);
-    $("#contact_message").attr("placeholder", random_char.message);
+  //random contact characters
+  var random_char = random_characters[Math.floor(Math.random()*random_characters.length)];
+  $("#contact_name").attr("placeholder", random_char.name);
+  $("#contact_email").attr("placeholder", random_char.email);
+  $("#contact_message").attr("placeholder", random_char.message);
 });
 
 //function to submit contact form
 function submitContact(){
-    if (can_submit) {
-        can_submit = false;
-        $("#submit-button").addClass('is-loading');
-        $.ajax({
-            url: "/contact",
-            data: {
-                contact_email: $("#contact_email").val(),
-                contact_name: $("#contact_name").val(),
-                contact_message: $("#contact_message").val()
-            },
-            method: "POST"
-        }).done(function(data){
-            $("#submit-button").removeClass('is-loading');
-            if (data.state == "error"){
-                can_submit = true;
-                $("#error_message").text(data.message).removeClass("is-hidden");
-            }
-            else {
-                $("input, textarea").not("#submit-button").val("").addClass('is-disabled');
-                $("#submit-button").text("Thank you!").addClass('is-disabled');
-            }
-            $("#message-success").removeClass("is-hidden").addClass("is-active");
-        });
+  $("#submit-button").addClass('is-loading');
+  $.ajax({
+    url: "/contact",
+    data: {
+      contact_email: $("#contact_email").val(),
+      contact_name: $("#contact_name").val(),
+      contact_message: $("#contact_message").val()
+    },
+    method: "POST"
+  }).done(function(data){
+    $("#submit-button").removeClass('is-loading');
+    $(".notification").addClass('is-hidden');
+    if (data.state == "error"){
+      $("#message-error-text").text(data.message);
+      $("#message-error").removeClass("is-hidden").addClass("is-active");
     }
+    else {
+      $("#contact-form").off();
+      $("input, textarea").not("#submit-button").val("").addClass('is-disabled');
+      $("#submit-button").addClass('is-disabled');
+      $("#message-success").removeClass("is-hidden").addClass("is-active");
+    }
+  });
 }
