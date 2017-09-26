e239add6b3c05ef512101e317260165f802ff2bd
Wonmin Lee <won2blee@gmail.com>
2017-09-26 18:36:09 -0400

    toggleable on compare tool too
    Former-commit-id: 37bc55c9b2c37d51b52a1852668c020e7d0bb6b3

--- server/routes/listings/listings_renter_functions.js
+++ server/routes/listings/listings_renter_functions.js
@@ -1233,6 +1233,7 @@ function getWhoIs(req, res, next, domain_name, unlisted){
         listing_info.dev_whois = whoisObj;
       }
 
+      //COMPARE TOOL VARIABLES
       //comparing, so make fake listing info
       if (req.query.compare == "true"){
         console.log("F: Rendering the comparison tool!");
@@ -1257,6 +1258,9 @@ function getWhoIs(req, res, next, domain_name, unlisted){
         listing_info.background_color = "#FFFFFF";
         listing_info.background_image = "";
         listing_info.logo = "";
+        listing_info.domain_age = 1;
+        listing_info.domain_appraisal = 1;
+        listing_info.social_sharing = 1;
         listing_info.history_module = 1;
         listing_info.traffic_module = 1;
         listing_info.info_module = 1;
