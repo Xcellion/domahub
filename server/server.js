8f294410780a2b51e8888b6750b9ada02bd1f7ef
Wonmin Lee <won2blee@gmail.com>
2017-09-28 10:02:10 -0400

    testing turning off CORS
    Former-commit-id: e1962ff2b6def6501a17140d379167a7da0daa56

--- server/server.js
+++ server/server.js
@@ -86,10 +86,10 @@ else {
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) {
   res.header('Access-DomaHub', true);
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.host);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Credentials', true);
+  // res.header('Access-Control-Allow-Origin', req.headers.host);
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 
