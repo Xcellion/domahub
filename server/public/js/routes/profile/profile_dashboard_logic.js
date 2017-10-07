86800addb1ca0c1a500c2f3d423df88dc79b00e8
wonkyu <wonq33@gmail.com>
2017-10-06 20:53:14 -0400

    you didnt even push ur shit
    Former-commit-id: 12afe67999c4608c51a321fa55275b1be68ba6db

--- server/public/js/routes/profile/profile_dashboard_logic.js
+++ server/public/js/routes/profile/profile_dashboard_logic.js
@@ -22,13 +22,67 @@ $(document).ready(function() {
     $(".user-dropdown-menu").toggleClass("is-hidden");
   });
 
+  //calculate and show # of unverified domains
   calcUnverified();
+
+  //populate the notifications tray
+  showNotifications();
+
+  //show empty notification if tray is empty
+  ifTrayEmpty();
+
+  //delete notification when you click its respective X
+  $(".delete").on("click", function() {
+    $(this).parent().remove();
+    ifTrayEmpty();
+  });
+
 });
 
+//find out how many domains are unverified
 function calcUnverified() {
   var counter = user.listings.filter(function(listing) {
     return listing.verified == null;
   });
 
   $("#unverified-counter").text(counter.length);
+}
+
+//populate the notifications tray
+function showNotifications() {
+
+  //if stripe payout settings are not set
+  if (!user.stripe_account) {
+    appendNotification("Complete your <a tabindex='0' class='is-underlined' href='/profile/settings#payout-address'>payout settings</a>.", true);
+  }
+
+  //if bank account is not connected
+  if (!(user.stripe_info && user.stripe_info.transfers_enabled)) {
+    appendNotification("Connect your <a tabindex='0' class='is-underlined' href='/profile/settings#payout-bank'>bank account</a>.", true);
+  }
+
+  //if domains are not verified
+  const counter = parseInt($("#unverified-counter").text());
+
+  if (counter > 0) {
+    appendNotification("Verify " + counter + " <a tabindex='0' class='is-underlined' href='/profile/mylistings'>unverified domains</a>.", true);
+  }
+}
+
+//when notifications tray is empty
+function ifTrayEmpty() {
+  if ($("#notifications-tray li").length == 0) {
+    appendNotification("Nothing to show - you're all set!", false);
+  }
+}
+
+function appendNotification(msg, delOption) {
+  var tray = $("#notifications-tray");
+
+  if (delOption) {
+    return tray.append("<li>" + "<span class='delete is-small is-transparent is-fc'></span>" + msg + "</li>");
+  }
+  else {
+    return tray.append("<li>" + msg + "</li>");
+  }
 }
