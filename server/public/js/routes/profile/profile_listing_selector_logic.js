3e37dfb9107340b4dcaccb055ae68d79af64e590
Wonmin Lee <won2blee@gmail.com>
2017-10-13 02:17:12 -0400

    dashboard functionality
    Former-commit-id: b80233939c1dd6e876c0bc3f6e261cb2ffecaea8

--- server/public/js/routes/profile/profile_listing_selector_logic.js
+++ server/public/js/routes/profile/profile_listing_selector_logic.js
@@ -72,6 +72,7 @@ $(document).ready(function(){
 
   //select dropper
   $("#selector-select-button").on('click', function(e){
+    console.log("WTF")
     $("#select-all-drop").toggleClass('is-hidden');
   });
 
@@ -191,6 +192,7 @@ function showEditor(url_tab){
   successMessage(false);
   $("#domain-selector").addClass('is-hidden');
   $("#domain-editor").removeClass('is-hidden');
+  leftMenuActive();
 }
 
 //<editor-fold>-------------------------------CREATE ROWS OF DOMAINS-------------------------------
@@ -512,79 +514,6 @@ function viewDomainDNS(){
   }
 
   //</editor-fold>
-
-//</editor-fold>
-
-//<editor-fold>-------------------------------URL HELPER FUNCTIONS--------------------------------
-
-//function to get a URL query param by name
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-  results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function updateQueryStringParam(key, value) {
-
-  var baseUrl = [location.protocol, '//', location.host, location.pathname].join(''),
-  urlQueryString = document.location.search,
-  newParam = key + '=' + value,
-  params = '?' + newParam;
-
-  // If the "search" string exists, then build params from it
-  if (urlQueryString) {
-
-    updateRegex = new RegExp('([\?&])' + key + '[^&]*');
-    removeRegex = new RegExp('([\?&])' + key + '=[^&;]+[&;]?');
-
-    if( typeof value == 'undefined' || value == null || value == '' ) { // Remove param if value is empty
-
-      params = urlQueryString.replace(removeRegex, "$1");
-      params = params.replace( /[&;]$/, "" );
-
-    } else if (urlQueryString.match(updateRegex) !== null) { // If param exists already, update it
-
-      params = urlQueryString.replace(updateRegex, "$1" + newParam);
-
-    } else { // Otherwise, add it to end of query string
-
-      params = urlQueryString + '&' + newParam;
-
-    }
-
-  }
-
-  params = (params == "?") ? "" : params;
-
-  window.history.replaceState({}, "", baseUrl + params);
-};
-
-function removeURLParameter(parameter) {
-  var url = [location.protocol, '//', location.host, location.pathname].join('');
-  var urlparts= window.location.href.split('?');
-  if (urlparts.length>=2) {
-
-    var prefix= encodeURIComponent(parameter)+'=';
-    var pars= urlparts[1].split(/[&;]/g);
-
-    //reverse iteration as may be destructive
-    for (var i= pars.length; i-- > 0;) {
-      //idiom for string.startsWith
-      if (pars[i].lastIndexOf(prefix, 0) !== -1) {
-        pars.splice(i, 1);
-      }
-    }
-
-    url= urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : "");
-    window.history.replaceState({}, "", url);
-  } else {
-    window.history.replaceState({}, "", url);
-  }
-}
 
 //</editor-fold>
 
