bcff79e78105b6ba517bebdf838514e073f1be00
Wonmin Lee <won2blee@gmail.com>
2018-01-09 13:56:40 -0500

    fixed dashboard no listings traffic bug, adding support for listing hub
    Former-commit-id: b09593c6ad5e05e53f717f5cd513babb87136738

---
+++ server/public/js/routes/listings/hub/hub_theming.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  //remove class to prevent screen flash DH green
+  $("#page-contents").removeClass('is-hidden');
+  $("#dh-footer").removeClass('is-hidden');
+
+});
+
+//return white or black text based on luminance
+function calculateLuminance(rgb) {
+  var hexValue = rgb.replace(/[^0-9A-Fa-f]/, '');
+  var r,g,b;
+  if (hexValue.length === 3) {
+    hexValue = hexValue[0] + hexValue[0] + hexValue[1] + hexValue[1] + hexValue[2] + hexValue[2];
+  }
+  if (hexValue.length !== 6) {
+    return 0;
+  }
+  r = parseInt(hexValue.substring(0,2), 16) / 255;
+  g = parseInt(hexValue.substring(2,4), 16) / 255;
+  b = parseInt(hexValue.substring(4,6), 16) / 255;
+
+  // calculate the overall luminance of the color
+  var luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+  if (luminance > 0.8) {
+    return "#1b2733";
+  }
+  else {
+    return "#fff";
+  }
+}
+
+function stylize(color, element, style, calculateluminance) {
+  if (color){
+    $(element).css(style, color);
+    if (style == "background-color" && calculateluminance) {
+      $(element).css("color", calculateLuminance(color));
+    }
+  }
+}
+
+//setup any custom premium colors
+function setupCustomColors(){
+  console.log("Setting up custom theme...");
+  // stylize(listing_info.primary_color, "#page-contents .is-primary:not(.notification)", "color");
+}
