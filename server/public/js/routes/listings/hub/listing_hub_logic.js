20e3e4a612fee690ef28dec4cac860d49a6146cf
Wonmin Lee <won2blee@gmail.com>
2018-01-09 21:18:54 -0500

    fixing selector checkbox form label, adding more support for hub
    Former-commit-id: e52bc1d153f99529e5b94ceaab426810ffdd68df

--- server/public/js/routes/listings/hub/listing_hub_logic.js
+++ server/public/js/routes/listings/hub/listing_hub_logic.js
@@ -1,8 +1,13 @@
+//<editor-fold>-----------------------------------------------------------------------------------VARIABLES
+
 var moneyFormat = wNumb({
   thousand: ',',
   prefix: '$'
 });
 var start_at = 0;
+var listings_per_page = 10;
+
+//</editor-fold>
 
 $(document).ready(function() {
 
@@ -21,8 +26,10 @@ $(document).ready(function() {
 
 });
 
-//search, sort, pagination handlers
-function filterHandlers(){
+//<editor-fold>-----------------------------------------------------------------------------------PAGE SETUP
+
+//search, sort, pagination, category, change layout handlers
+function setupHandlers(){
 
   //sort handler
   $(".sort-header").off().on("click", function(){
@@ -51,10 +58,47 @@ function filterHandlers(){
     createDomaintable(listing_info.listings);
   });
 
+  //set pagnination buttons values
+  $("#prev-page").val(0 - listings_per_page);
+  $("#next-page").val(listings_per_page);
+
   //pagination handler
   $(".page-button").off().on('click', function(){
-    start_at += $(this).data('val');
+    start_at += parseFloat($(this).val());
     createDomaintable();
+  });
+
+  //build hash table for all categories (front v back)
+  var all_categories = {};
+  categories.forEach(function(category){
+    for (var x in category){
+      all_categories[category.back] = category.front
+    }
+  });
+
+  //build categories dropdown
+  var categories_array = listing_info.listings.reduce(function(a, c, i){
+    if (c.categories){
+      return a.concat(c.categories.split(" "));
+    }
+    else {
+      return a;
+    }
+  }, []);
+  categories_array = arrayUnique(categories_array).sort();
+  for (var x = 0 ; x < categories_array.length ; x++){
+    $("#categories-select").append("<option value=" + categories_array[x] + ">" + all_categories[categories_array[x]] + "</option>");
+  }
+
+  //categories handler
+  $("#categories-select").off().on('input', function(){
+    createDomaintable();
+  });
+
+  //layout handler
+  $("#layout-button").off().on("click", function(){
+    var this_svg = ($(this).find("svg").attr("data-icon") == "th-large") ? "align-justify" : "th-large";
+    $(this).find("svg").attr("data-icon", this_svg);
   });
 
 }
@@ -69,7 +113,7 @@ function findOtherDomains(){
         owner_id: listing_info.owner_id,
         domain_name_exclude: listing_info.domain_name,
         sort_by : "id",
-        total : 10,
+        total : listings_per_page,
         starting_id : false
       }
     }).done(function(data){
@@ -80,7 +124,7 @@ function findOtherDomains(){
         listing_info.listings = [];
       }
 
-      filterHandlers();
+      setupHandlers();
       createDomaintable();
     });
   }
@@ -89,28 +133,37 @@ function findOtherDomains(){
   }
 }
 
+//</editor-fold>
+
+//<editor-fold>-----------------------------------------------------------------------------------TABLE SETUP
+
 //create the table of domains owned (depending on sort and search)
 function createDomaintable(){
 
   //sort the listings by sort_by
   var sort_by = $(".sort-header.is-active").data('sort-by');
   var sort_direction = $(".sort-header.is-active").data('sort-direction');
-  var listings_to_show = listing_info.listings.sort(function(a, b){
-    if (sort_by == "price_rate"){
-      var a_sort = a[sort_by];
-      var b_sort = b[sort_by];
-    }
-    else {
-      var a_sort = a[sort_by];
-      var b_sort = b[sort_by];
-    }
-    if (sort_direction){
-      return (a_sort > b_sort) ? 1 : (a_sort < b_sort) ? -1 : 0;
-    }
-    else {
-      return (a_sort > b_sort) ? -1 : (a_sort < b_sort) ? 1 : 0;
-    }
-  });
+  if (typeof sort_by != "undefined" && typeof sort_direction != "undefined"){
+    var listings_to_show = listing_info.listings.sort(function(a, b){
+      if (sort_by == "price_rate"){
+        var a_sort = a[sort_by];
+        var b_sort = b[sort_by];
+      }
+      else {
+        var a_sort = a[sort_by];
+        var b_sort = b[sort_by];
+      }
+      if (sort_direction){
+        return (a_sort > b_sort) ? 1 : (a_sort < b_sort) ? -1 : 0;
+      }
+      else {
+        return (a_sort > b_sort) ? -1 : (a_sort < b_sort) ? 1 : 0;
+      }
+    });
+  }
+  else {
+    var listings_to_show = listing_info.listings
+  }
 
   //filter by search
   listings_to_show = listings_to_show.filter(function(listing){
@@ -120,12 +173,24 @@ function createDomaintable(){
     }
   });
 
+  //filter by categories
+  listings_to_show = listings_to_show.filter(function(listing){
+    if (listing.categories.indexOf($("#categories-select").val()) != -1){
+      return true;
+    }
+  });
+
+  //double check pagination
+  if (start_at > listings_to_show.length){
+    start_at = 0;
+  }
+
   //create table
   if (listings_to_show.length > 0){
 
     $(".table-row:not(#clone-row)").remove();
 
-    for (var x = 0; x < listings_to_show.slice(start_at, start_at + 10).length; x++){
+    for (var x = 0; x < listings_to_show.slice(start_at, start_at + listings_per_page).length; x++){
       var clone_row = $("#clone-row").clone().removeAttr("id");
       if (listings_to_show[start_at + x].logo){
         clone_row.find(".domain-row-logo").attr("src", listings_to_show[start_at + x].logo);
@@ -141,16 +206,15 @@ function createDomaintable(){
       else {
         clone_row.find(".bin-col .control-item").addClass('is-hidden');
       }
-
       if (listings_to_show[start_at + x].min_price){
-        clone_row.find(".domain-row-bin").text(moneyFormat.to(listings_to_show[start_at + x].min_price));
+        clone_row.find(".domain-row-min").text(moneyFormat.to(listings_to_show[start_at + x].min_price));
       }
       else {
         clone_row.find(".min-col .control-item").addClass('is-hidden');
       }
 
       if (listings_to_show[start_at + x].price_rate && listings_to_show[start_at + x].price_type && listings_to_show[start_at + x].rentable){
-        clone_row.find(".domain-row-bin").text(moneyFormat.to(listings_to_show[start_at + x].price_rate) + " / " + listings_to_show[start_at + x].price_type);
+        clone_row.find(".domain-row-rent").text(moneyFormat.to(listings_to_show[start_at + x].price_rate) + " / " + listings_to_show[start_at + x].price_type);
       }
       else {
         clone_row.find(".rent-col .control-item").addClass('is-hidden');
@@ -169,7 +233,7 @@ function createDomaintable(){
     $("#domains-table, .table-row:not(#clone-row)").removeClass('is-hidden');
 
     //how many domains text
-    $("#total-domain-count").text("Showing " + listings_to_show.slice(start_at, start_at + 10).length + " of " + listing_info.listings.length + " domains");
+    $("#total-domain-count").text("Showing " + (start_at + 1) + " - " + (start_at + listings_to_show.slice(start_at, start_at + listings_per_page).length) + " of " + listings_to_show.length + " domains");
 
     //show prev
     $(".page-button").addClass('is-disabled');
@@ -178,7 +242,7 @@ function createDomaintable(){
     }
 
     //show next
-    if (start_at + 10 < listing_info.listings.length) {
+    if (start_at + listings_per_page < listings_to_show.length) {
       $("#next-page").removeClass('is-disabled');
     }
   }
@@ -190,3 +254,21 @@ function createDomaintable(){
     $("#total-domain-count").text("");
   }
 }
+
+//</editor-fold>
+
+//<editor-fold>-----------------------------------------------------------------------------------HELPERS
+
+//makes an array unique
+function arrayUnique(array) {
+    var a = array.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+    return a;
+}
+
+//</editor-fold>
